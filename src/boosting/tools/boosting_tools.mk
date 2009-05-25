3d0deee34b59202c0a87f4f59f635d3e579b6f20
jeremy@jeremy-desktop <none@none>
2009-05-25 15:18:39 -0400

    Added CUDA dependency to the boosting library; will need compile-time detection to do this reliably in the future

--- src/boosting/tools/boosting_tools.mk
+++ src/boosting/tools/boosting_tools.mk
@@ -7,5 +7,5 @@ LIBBOOSTING_TOOLS_LINK :=	utils db arch boost_regex-mt
 $(eval $(call library,boosting_tools,$(LIBBOOSTING_TOOLS_SOURCES),$(LIBBOOSTING_TOOLS_LINK)))
 
 
-$(eval $(call program,classifier_training_tool,boosting boosting_tools utils ACE boost_program_options-mt boost_regex-mt,,tools))
+$(eval $(call program,classifier_training_tool,boosting boosting_tools utils ACE boost_program_options-mt boost_regex-mt boosting_cuda,,tools))
 
