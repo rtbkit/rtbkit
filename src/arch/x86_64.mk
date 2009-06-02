9734f61156eebd865c32c86e7d824ecbacd03c4a
jeremy@jeremy-desktop <none@none>
2009-06-02 13:04:44 -0400

    Added CXXDEBUGFLAGS

--- src/arch/x86_64.mk
+++ src/arch/x86_64.mk
@@ -9,6 +9,7 @@ CXX := colorccache g++
 CXXFLAGS := -I. -pipe -Wall -Werror -Wno-sign-compare -Woverloaded-virtual -O3 -fPIC -m64 -g -I/usr/include/eigen2 -I$(CUDA_INCLUDE_PATH) -I$(CAL_INCLUDE_PATH)
 CXXLINKFLAGS := -shared -L$(BIN) -L$(CUDA_LIBRARY_PATH) -Wl,--rpath,$(BIN),--rpath,$(CUDA_LIBRARY_PATH)
 CXXEXEFLAGS :=	$(if $(MEMORY_ALLOC_LIBRARY),-l$(MEMORY_ALLOC_LIBRARY)) -L$(BIN) -L$(CUDA_LIBRARY_PATH) -Wl,--rpath,$(BIN),--rpath,$(CUDA_LIBRARY_PATH)
+CXXDEBUGFLAGS := -O0 -g
 
 FC := colorccache gfortran
 FFLAGS := -I. -fPIC
