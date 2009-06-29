d9b1faa47af202b66c09c82f38f9c5c3e6f4a2da
jeremy@jeremy-desktop <none@none>
2009-06-28 23:03:13 -0400

    Fixed one problem (inhibit/fire) in backprop cuda; still looking for other one

--- src/arch/cuda.mk
+++ src/arch/cuda.mk
@@ -6,7 +6,7 @@ CUDA_LIBRARY_PATH := $(CUDA_PATH)/lib
 # Set to 1 to emulate the CUDA device
 CUDA_DEVICE_EMULATION ?= 0
 
-NVCC_EMULATION       := -Xcompiler -fno-access-control --device-emulation # -Xcudafe --rtti,--exceptions
+NVCC_EMULATION       := -Xcompiler -fno-access-control --device-emulation -Xcudafe --rtti,--exceptions,--g++
 
 NVCC       := $(CUDA_INSTALL_PATH)/bin/nvcc -D_FORTIFY_SOURCE=0 -shared -Xcompiler -fPIC,-g,-O3 -arch=sm_13 --ptxas-options=-v $(if $(findstring x1x,x$(CUDA_DEVICE_EMULATION)x),$(NVCC_EMULATION))
 
