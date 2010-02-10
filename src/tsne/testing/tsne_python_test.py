5f4e39f1ef1e7fb9dd722086fbb4368d428a5bcb
jeremy_barnes <none@none>
2010-02-09 21:51:02 -0500

    improved t-SNE python testing: prettier graph; command line switches

--- src/tsne/testing/tsne_python_test.py
+++ src/tsne/testing/tsne_python_test.py
@@ -51,22 +51,43 @@ def test_vectors_to_distances():
 
 test_vectors_to_distances()
 
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-s", "--show-graph", dest="show_graph",
+                  action="store_true", default=False,
+                  help="display the graph once it's been generated")
+
+parser.add_option("-a", "--all-data", dest="all_data",
+                  action="store_true", default=False,
+                  help="use all data (not just 250 rows)")
+
+(options, args) = parser.parse_args()
 
 def test_tsne():
     global digits, labels
 
     nrows = digits.shape[0];
     # Smaller number of labels for debugging...
-    nrows = 250
+    if not options.all_data:
+        nrows = 250
 
     X = digits[range(nrows), ...]
     L = labels[range(nrows), ...]
 
     Y = tsne.tsne(X, 2, 50, 20.0, use_pca=True, max_iter=1000)
     #Y = tsne.tsne(X, 2, 50, 20.0, use_pca=False)
-    Plot.scatter(Y[:,0], Y[:,1], 20, L)
-    Plot.legend(loc='lower left')
-    #Plot.show()
+
+    for i in xrange(10):
+        idxs = [idx for idx in xrange(len(L)) if L[idx] == i]
+        c = Plot.get_cmap()(0.1 * i)
+        Plot.scatter(Y[idxs,0], Y[idxs,1], 20, c, label="%d" % i)
+    #Plot.axis('off')
+    Plot.xticks([])
+    Plot.yticks([])
+    Plot.legend(loc='upper left', scatterpoints=1)
+    if options.show_graph:
+        Plot.show()
 
 
 test_tsne()
