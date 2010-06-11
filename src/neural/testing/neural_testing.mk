9d0f1ca0b431f38c84cf7b1395a8aa91df9d8c3d
jeremy_barnes <none@none>
2010-06-10 23:12:22 -0400

    all tests now compile

--- src/neural/testing/neural_testing.mk
+++ src/neural/testing/neural_testing.mk
@@ -3,10 +3,10 @@
 # Copyright (c) 2009 Jeremy Barnes.  All rights reserved.
 
 $(eval $(call test,parameters_test,neural,boost))
-$(eval $(call test,dense_layer_test,neural,boost))
-$(eval $(call test,layer_stack_test,neural,boost))
+$(eval $(call test,dense_layer_test,neural utils arch db worker_task,boost))
+$(eval $(call test,layer_stack_test,neural utils arch db worker_task,boost))
 $(eval $(call test,discriminative_trainer_test,neural,boost))
-$(eval $(call test,twoway_layer_test,neural,boost))
-$(eval $(call test,perceptron_test,neural,boost))
+$(eval $(call test,twoway_layer_test,neural utils arch db worker_task,boost))
+$(eval $(call test,perceptron_test,neural utils boosting worker_task,boost))
 $(eval $(call test,output_encoder_test,neural,boost))
 
--- src/tsne/testing/tsne_testing.mk
+++ src/tsne/testing/tsne_testing.mk
@@ -1,2 +1,2 @@
-$(eval $(call test,tsne_test,tsne,boost timed))
+$(eval $(call test,tsne_test,tsne utils arch,boost timed))
 $(eval $(call pytest,tsne_python_test,tsne))
