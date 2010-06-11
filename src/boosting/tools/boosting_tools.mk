3d4637fc271f73ab84a490b3e83b4c60afeaad64
jeremy_barnes <none@none>
2010-06-11 14:12:56 -0400

    fixed building of tools

--- src/boosting/tools/boosting_tools.mk
+++ src/boosting/tools/boosting_tools.mk
@@ -7,7 +7,7 @@ LIBBOOSTING_TOOLS_LINK :=	utils db arch boost_regex-mt
 $(eval $(call library,boosting_tools,$(LIBBOOSTING_TOOLS_SOURCES),$(LIBBOOSTING_TOOLS_LINK)))
 
 
-$(eval $(call program,classifier_training_tool,boosting boosting_tools utils ACE boost_program_options-mt boost_regex-mt $(if $(findstring 1,$(CUDA_ENABLED)), boosting_cuda),,tools))
+$(eval $(call program,classifier_training_tool,boosting boosting_tools utils arch worker_task ACE boost_program_options-mt boost_regex-mt $(if $(findstring 1,$(CUDA_ENABLED)), boosting_cuda),,tools))
 
-$(eval $(call program,training_data_tool,boosting boosting_tools utils ACE boost_program_options-mt boost_regex-mt,,tools))
+$(eval $(call program,training_data_tool,boosting boosting_tools utils arch ACE boost_program_options-mt boost_regex-mt worker_task,,tools))
 
