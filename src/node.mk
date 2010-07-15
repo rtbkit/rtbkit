73f4f74af5a7e5a4782fe05109bd1129ff4c739d
jeremy_barnes <none@none>
2010-07-15 16:31:20 -0400

    added ability to pass node command line options to node tests

--- src/node.mk
+++ src/node.mk
@@ -16,21 +16,21 @@ endef
 
 # $(1) name of the test (the javascript file that contains the test case)
 # $(2) node.js modules on which it depends
-# $(3) test style.  Currently unused.
+# $(3) options to the node executable
 
 define nodejs_test
 $$(if $(trace),$$(warning called nodejs_test "$(1)" "$(2)" "$(3)"))
 
-TEST_$(1)_COMMAND := rm -f $(TESTS)/$(1).{passed,failed} && ((set -o pipefail && NODE_PATH=$(NODE_PATH) $(NODE) $(CWD)/$(1).js > $(TESTS)/$(1).running 2>&1 && mv $(TESTS)/$(1).running $(TESTS)/$(1).passed) || (mv $(TESTS)/$(1).running $(TESTS)/$(1).failed && echo "           $(1) FAILED" && cat $(TESTS)/$(1).failed && false))
+TEST_$(1)_COMMAND := rm -f $(TESTS)/$(1).{passed,failed} && ((set -o pipefail && NODE_PATH=$(NODE_PATH) $(NODE) $(3) $(CWD)/$(1).js > $(TESTS)/$(1).running 2>&1 && mv $(TESTS)/$(1).running $(TESTS)/$(1).passed) || (mv $(TESTS)/$(1).running $(TESTS)/$(1).failed && echo "           $(1) FAILED" && cat $(TESTS)/$(1).failed && false))
 
-TEST_$(1)_DEPS := $$(foreach lib,$(2),$$(warning lib $$(lib) deps $$(LIB_$$(lib)_DEPS))$$(if $$(LIB_$$(lib)_DEPS),$$(LIB_$$(lib)_DEPS),$$(error variable LIB_$$(lib)_DEPS for library $(lib) in test $(1) is empty)))
+TEST_$(1)_DEPS := $$(foreach lib,$(2),$$(if $$(LIB_$$(lib)_DEPS),$$(LIB_$$(lib)_DEPS),$$(error variable LIB_$$(lib)_DEPS for library $(lib) in test $(1) is empty)))
 
 $(TESTS)/$(1).passed:	$(CWD)/$(1).js $$(TEST_$(1)_DEPS)
 	$$(if $(verbose_build),@echo '$$(TEST_$(1)_COMMAND)',@echo "[TESTCASE] $(1)")
 	@$$(TEST_$(1)_COMMAND)
 
 $(1):	$(CWD)/$(1).js $$(TEST_$(1)_DEPS)
-	NODE_PATH=$(NODE_PATH) $(NODE) $(CWD)/$(1).js
+	NODE_PATH=$(NODE_PATH) $(NODE) $(3) $(CWD)/$(1).js
 
 .PHONY: $(1)
 
