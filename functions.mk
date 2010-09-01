370f266d7064b15c11064981508f02c4ad05283b
jeremybarnes <jeremy@recoset.com>
2010-09-01 16:08:26 -0400

    fixed exception_hook make errors

--- functions.mk
+++ functions.mk
@@ -219,7 +219,7 @@ $(1)_OBJFILES:=$$(foreach file,$$(addsuffix .lo,$$(basename $$($(1)_PROGFILES:%=
 LINK_$(1)_COMMAND:=$$(CXX) $$(CXXFLAGS) $$(CXXEXEFLAGS) -o $(BIN)/$(1) -lexception_hook -ldl $$(foreach lib,$(2), -l$$(lib)) $$($(1)_OBJFILES) $$(CXXEXEPOSTFLAGS)
 
 
-$(BIN)/$(1):	$(BIN)/.dir_exists $$($(1)_OBJFILES) $$(foreach lib,$(2),$$(LIB_$$(lib)_DEPS)) $$(if $$(BUILD_arch/exception_hook.lo_OBJ),$$(BIN)/libexception_hook.so)
+$(BIN)/$(1):	$(BIN)/.dir_exists $$($(1)_OBJFILES) $$(foreach lib,$(2),$$(LIB_$$(lib)_DEPS)) $$(if $$(HAS_EXCEPTION_HOOK),$$(BIN)/libexception_hook.so)
 	$$(if $(verbose_build),@echo $$(LINK_$(1)_COMMAND),@echo "[BIN] $(1)")
 	@$$(LINK_$(1)_COMMAND)
 
@@ -253,7 +253,7 @@ $(1)_OBJFILES:=$$(BUILD_$(CWD)/$(1).lo_OBJ)
 
 LINK_$(1)_COMMAND:=$$(CXX) $$(CXXFLAGS) $$(CXXEXEFLAGS) -o $(TESTS)/$(1) -lexception_hook -ldl $$(foreach lib,$(2), -l$$(lib)) $$($(1)_OBJFILES) $(if $(findstring boost,$(3)), -lboost_unit_test_framework-mt)
 
-$(TESTS)/$(1):	$(TESTS)/.dir_exists  $$($(1)_OBJFILES) $$(foreach lib,$(2),$$(LIB_$$(lib)_DEPS)) $$(if $$(BUILD_arch/exception_hook.lo_OBJ),$$(BIN)/libexception_hook.so)
+$(TESTS)/$(1):	$(TESTS)/.dir_exists  $$($(1)_OBJFILES) $$(foreach lib,$(2),$$(LIB_$$(lib)_DEPS)) $$(if $$(HAS_EXCEPTION_HOOK),$$(BIN)/libexception_hook.so)
 	$$(if $(verbose_build),@echo $$(LINK_$(1)_COMMAND),@echo "[BIN] $(1)")
 	@$$(LINK_$(1)_COMMAND)
 
