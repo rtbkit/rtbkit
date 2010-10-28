280063efdb15e2d4b124b5b63b9a21910fc72b0c
jeremybarnes <jeremy@recoset.com>
2010-10-27 23:58:18 -0400

    minor adjustments to node executables

--- node.mk
+++ node.mk
@@ -1,6 +1,7 @@
 ifeq ($(NODEJS_ENABLED),1)
 
-NODE ?= LD_PRELOAD=$(BIN)/libexception_hook.so node
+NODE ?= node
+NODE_PRELOAD ?= LD_PRELOAD=$(BIN)/libexception_hook.so
 VOWS ?= /usr/local/bin/vows
 NODE_PATH := $(if $(NODE_PATH),$(NODE_PATH):)$(BIN)
 NODE_TEST_DEPS ?= $(BIN)/libexception_hook.so
@@ -28,7 +29,7 @@ endef
 define nodejs_test
 $$(if $(trace),$$(warning called nodejs_test "$(1)" "$(2)" "$(3)" "$(4)" "$(5)"))
 
-TEST_$(1)_COMMAND := rm -f $(TESTS)/$(1).{passed,failed} && ((set -o pipefail && NODE_PATH=$(NODE_PATH) $(NODE) $(3) $(CWD)/$(1).js > $(TESTS)/$(1).running 2>&1 && mv $(TESTS)/$(1).running $(TESTS)/$(1).passed) || (mv $(TESTS)/$(1).running $(TESTS)/$(1).failed && echo "           $(COLOR_RED)$(1) FAILED$(COLOR_RESET)" && cat $(TESTS)/$(1).failed && echo "           $(COLOR_RED)$(1) FAILED$(COLOR_RESET)" && false))
+TEST_$(1)_COMMAND := rm -f $(TESTS)/$(1).{passed,failed} && ((set -o pipefail && NODE_PATH=$(NODE_PATH) $(NODE_PRELOAD) $(NODE) $(3) $(CWD)/$(1).js > $(TESTS)/$(1).running 2>&1 && mv $(TESTS)/$(1).running $(TESTS)/$(1).passed) || (mv $(TESTS)/$(1).running $(TESTS)/$(1).failed && echo "           $(COLOR_RED)$(1) FAILED$(COLOR_RESET)" && cat $(TESTS)/$(1).failed && echo "           $(COLOR_RED)$(1) FAILED$(COLOR_RESET)" && false))
 
 TEST_$(1)_DEPS := $$(foreach lib,$(2),$$(if $$(LIB_$$(lib)_DEPS),$$(LIB_$$(lib)_DEPS),$$(error variable LIB_$$(lib)_DEPS for library $(lib) in test $(1) is empty)))
 
@@ -38,7 +39,7 @@ $(TESTS)/$(1).passed:	$(CWD)/$(1).js $$(TEST_$(1)_DEPS) $(NODE_TEST_DEPS)
 	$$(if $(verbose_build),@echo '$$(TEST_$(1)_COMMAND)',@echo "           $(COLOR_GREEN)$(1) passed$(COLOR_RESET)")
 
 $(1):	$(CWD)/$(1).js $$(TEST_$(1)_DEPS)
-	NODE_PATH=$(NODE_PATH) $(NODE) $(3) $(CWD)/$(1).js
+	NODE_PATH=$(NODE_PATH) $(NODE_PRELOAD) $(NODE) $(3) $(CWD)/$(1).js
 
 .PHONY: $(1)
 
@@ -55,10 +56,10 @@ define nodejs_program
 
 NODE_PROGRAM_$(1)_DEPS := $$(foreach lib,$(2),$$(if $$(LIB_$$(lib)_DEPS),$$(LIB_$$(lib)_DEPS),$$(error variable LIB_$$(lib)_DEPS for library $(lib) in test $(1) is empty)))
 
-$(BIN)/$(1):	$(CWD)/$(1).js $(NODE_TEST_DEPS)
+$(BIN)/$(1):	$(CWD)/$(1).js $$(NODE_PROGRAM_$(1)_DEPS) $(NODE_TEST_DEPS)
 	@echo "[NODEJS] $(1)"
 	@echo "#!/usr/bin/env bash" > $$@~
-	@echo -n "//usr/bin/env NODE_PATH=$(NODE_PATH) $(NODE) " >> $$@~
+	@echo -n "//usr/bin/env NODE_PATH=$(NODE_PATH) $(NODE_PRELOAD) $(NODE) " >> $$@~
 	@echo -n $$$$ >> $$@~
 	@echo -n '0 "' >> $$@~
 	@echo -n $$$$ >> $$@~
