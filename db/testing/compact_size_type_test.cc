0715716b83db9d088f91bafeb06f333d34290ea5
Jeremy Barnes <jeremy@recoset.com>
2012-05-11 09:54:53 -0400

    fixed 64 bit compact_size_t test

--- db/testing/compact_size_type_test.cc
+++ db/testing/compact_size_type_test.cc
@@ -13,6 +13,7 @@
 #include "jml/utils/guard.h"
 #include "jml/db/persistent.h"
 #include "jml/db/compact_size_types.h"
+#include "jml/utils/hex_dump.h"
 #include <boost/test/unit_test.hpp>
 #include <boost/bind.hpp>
 #include <sstream>
@@ -33,12 +34,20 @@ void test_compact_size_type(uint64_t value)
         store << cs;
     }
 
-    cerr << "os.str().size() = " << os.str().size() << endl;
+    //cerr << "os.str().size() = " << os.str().size() << endl;
 
     std::istringstream is(os.str());
     DB::Store_Reader store(is);
 
     compact_size_t cs(store);
+
+    if (cs.size_ != value) {
+        cerr << "value:      ";
+        hex_dump(&value, 8);
+        cerr << "serialized: ";
+        string s = os.str();
+        hex_dump(s.c_str(), s.length());
+    }
 
     BOOST_CHECK_EQUAL(cs.size_, value);
 }
@@ -49,9 +58,7 @@ void test_compact_size_type(uint64_t value)
 
 BOOST_AUTO_TEST_CASE( test1 )
 {
-    // TODO: should go to 64, but problem that I don't have time to look at...
-
-    for (unsigned i = 0;  i < 63;  ++i) {
+    for (unsigned i = 0;  i < 64;  ++i) {
         unsigned long long val = 1ULL << i;
 
         cerr << "testing i = " << i << endl;
@@ -60,5 +67,14 @@ BOOST_AUTO_TEST_CASE( test1 )
         test_compact_size_type(val);
         test_compact_size_type(val + 1);
     }
+
+    cerr << "testing 64 bit values" << endl;
+    test_compact_size_type(0xf000000000000000ULL);
+    test_compact_size_type(0xf000000000000001ULL);
+    test_compact_size_type(0xff00000000000000ULL);
+    test_compact_size_type(0xfff0000000000000ULL);
+    test_compact_size_type(0xffff000000000000ULL);
+    test_compact_size_type(0xfffffffffffffffeULL);
+    test_compact_size_type(0xffffffffffffffffULL);
 }
 
