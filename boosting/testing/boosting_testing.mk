a0d5f6b89b36af09f9b50781e3664aa0f1997ed7
Francois Maillet <francois@datacratic.com>
2012-10-16 17:41:36 -0400

    Added dataset nan failing program

--- boosting/testing/boosting_testing.mk
+++ boosting/testing/boosting_testing.mk
@@ -5,6 +5,8 @@ $(eval $(call test,decision_tree_unlimited_depth_test,boosting utils arch worker
 $(eval $(call test,glz_classifier_test,boosting utils arch worker_task,boost))
 $(eval $(call test,probabilizer_test,boosting utils arch,boost))
 
+$(eval $(call program,dataset_nan_test,boosting utils arch boosting_tools))
+
 ifeq ($(CUDA_ENABLED),1)
 $(eval $(call test,split_cuda_test,boosting_cuda,boost))
 endif # CUDA_ENABLED
