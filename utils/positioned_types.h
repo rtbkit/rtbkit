0ca60296f0e440df95276eaa874e7122ba64e2bd
Jeremy Barnes <jeremy@datacratic.com>
2012-11-13 11:22:07 -0500

    added more positioned type utilities

--- utils/positioned_types.h
+++ utils/positioned_types.h
@@ -1,3 +1,10 @@
+/* positioned_types.h                                              -*- C++ -*-
+   Jeremy Barnes, 13 November 2012
+   Copyright (c) 2012 Datacratic Inc.
+
+   Public domain.
+*/
+
 #ifndef __ml__positioned_types_h__
 #define __ml__positioned_types_h__
 
@@ -44,6 +51,90 @@ template<int Index>
 struct MakeInPositionList<Index> {
     typedef TypeList<> List;
 };
+
+
+/*****************************************************************************/
+/* EXTRACT ARG AT POSITION                                                   */
+/*****************************************************************************/
+
+/** Given an integer index and a list of types, this will extract both the
+    type and the value of the argument at the given index.
+*/
+template<int Current, int Index,
+         typename... Params>
+struct ExtractArgAtPosition {
+};
+
+/** End of recursion... we have made it to where we need to be */
+template<int Current, typename Head, typename... Rest>
+struct ExtractArgAtPosition<Current, Current, Head, Rest...> {
+    // This is the one we need
+    typedef Head type;
+    static Head extract(Head arg, Rest... rest)
+    {
+        return arg;
+    }
+};
+
+/** Recursive version */
+template<int Current, int Index,
+         typename Head, typename... Rest>
+struct ExtractArgAtPosition<Current, Index, Head, Rest...> {
+    typedef typename ExtractArgAtPosition<Current + 1, Index, Rest...>::type type;
+
+    // Recurse to get a later argument
+    static type
+    extract(Head arg, Rest... rest)
+    {
+        return ExtractArgAtPosition<Current + 1, Index, Rest...>
+            ::extract(std::forward<Rest>(rest)...);
+    }
+};
+
+
+/*****************************************************************************/
+/* POSITIONED DUAL TYPE                                                      */
+/*****************************************************************************/
+
+/** A container metaclass that holds a position (index) and two types. */
+
+template<int Index, typename T1, typename T2>
+struct PositionedDualType {
+};
+
+/** A template that constructs a TypeList of PositionedDualType values
+    from two TypeLists.  It's kind of like a zip function for two lists
+    of types:
+
+    <t0, t1, t2, t3>, <u0, u1, u2, u3>, <0, 1, 2, 3> -->
+        < <t0, u0, 0>, <t1, u1, 1>, <t2, u2, 2>, <t3, u3, 3> >
+*/
+
+template<int Index, typename TypeList1, typename TypeList2>
+struct PositionedDualTypeList {
+};
+
+template<int Index,
+         typename Head1, typename... Tail1,
+         typename Head2, typename... Tail2>
+struct PositionedDualTypeList<Index,
+                              ML::TypeList<Head1, Tail1...>,
+                              ML::TypeList<Head2, Tail2...> > {
+    typedef PositionedDualTypeList<Index + 1,
+                                   ML::TypeList<Tail1...>,
+                                   ML::TypeList<Tail2...> > Base;
+typedef typename ML::PushFront<PositionedDualType<Index, Head1, Head2>,
+                               typename Base::List>::List List;
+};
+
+template<int Index>
+struct PositionedDualTypeList<Index,
+                              ML::TypeList<>,
+                              ML::TypeList<> > {
+    typedef ML::TypeList<> List;
+};
+
+
 
 } // namespace ML
 
