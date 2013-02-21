290bcf8ce3584cff070778c7fc93f0a13b69b89e
Wolfgang Sourdeau <wolfgang@datacratic.com>
2013-02-21 15:56:46 -0500

    fixed link variable name

--- plugins/exchange/exchange.mk
+++ plugins/exchange/exchange.mk
@@ -6,7 +6,7 @@ LIBRTB_EXCHANGE_SOURCES := \
 	http_auction_handler.cc \
 	ad_server_connector.cc
 
-LIBRTB_ROUTER_LINK := \
+LIBRTB_EXCHANGE_LINK := \
 	zeromq boost_thread
 
 $(eval $(call library,exchange,$(LIBRTB_EXCHANGE_SOURCES),$(LIBRTB_EXCHANGE_LINK)))
