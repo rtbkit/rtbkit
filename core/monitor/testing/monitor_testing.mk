23bdfd326e01748d01c0a6e17fc813bbf9eff116
Wolfgang Sourdeau <wolfgang@datacratic.com>
2013-02-21 15:58:31 -0500

    restored monitor_behaviour_test

--- core/monitor/testing/monitor_testing.mk
+++ core/monitor/testing/monitor_testing.mk
@@ -4,13 +4,15 @@
 # Monitor
 $(eval $(call test,monitor_test,monitor_service,boost))
 $(eval $(call test,monitor_provider_proxy_test,monitor_service,boost))
-#$(eval $(call test,monitor_behaviour_test,monitor monitor_service,boost))
 
 # Monitor Provider
 $(eval $(call test,monitor_provider_test,monitor,boost))
 
 # Monitor Proxy
 $(eval $(call test,monitor_proxy_test,monitor,boost))
+
+# Integration test between all Monitor components
+$(eval $(call test,monitor_behaviour_test,monitor monitor_service,boost))
 
 monitor_tests: monitor_test monitor_provider_test monitor_provider_proxy_test\
 	 monitor_proxy_test monitor_behaviour_test
