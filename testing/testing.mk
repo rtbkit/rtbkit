a953f0e149825d936aec989604b665dbb13442ce
Jeremy Barnes <jeremy@datacratic.com>
2013-03-24 22:55:37 -0400

    added test of historical bid request

--- testing/testing.mk
+++ testing/testing.mk
@@ -9,6 +9,7 @@
 $(eval $(call vowscoffee_test,bid_request_test,bid_request))
 $(eval $(call test,agent_configuration_test,rtb_router bidding_agent,boost))
 $(eval $(call test,augmentation_list_test,rtb,boost))
+$(eval $(call test,historical_bid_request_test,bid_request,boost))
 
 $(eval $(call library,integration_test_utils,generic_exchange_connector.cc mock_exchange.cc,rtb_router exchange))
 
