1a33bf73cc24da254f742e1391aa6a995b3249ac
Christine Soon Desmarais <christine@datacratic.com>
2013-06-04 18:54:14 +0000

    made inclusion absolute

--- utils/testing/variadic_hash_test.cc
+++ utils/testing/variadic_hash_test.cc
@@ -2,7 +2,7 @@
 #define BOOST_TEST_DYN_LINK
 
 #include <boost/test/unit_test.hpp>
-#include <utils/variadic_hash.h>
+#include <soa/utils/variadic_hash.h>
 #include <iostream>
 
 using namespace std;
