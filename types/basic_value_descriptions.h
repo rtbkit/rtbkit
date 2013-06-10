6bbab8bf41f18a459ad264e00c9e1186ab8e33b5
Wolfgang Sourdeau <wolfgang@datacratic.com>
2013-06-10 09:53:13 -0400

    ensure that we represent 128 bit integer ids as string

--- types/basic_value_descriptions.h
+++ types/basic_value_descriptions.h
@@ -30,8 +30,9 @@ struct DefaultDescription<Datacratic::Id>
     virtual void printJsonTyped(const Datacratic::Id * val,
                                 JsonPrintingContext & context) const
     {
-        if (val->type == Id::Type::INT64DEC
-            || val->type == Id::Type::BIGDEC) {
+        if (val->val2 == 0
+            && (val->type == Id::Type::INT64DEC
+                || val->type == Id::Type::BIGDEC)) {
             context.writeUnsignedLongLong(val->toInt());
         }
         else {
