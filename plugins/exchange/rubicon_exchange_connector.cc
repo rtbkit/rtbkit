a3850c05fe9e18058f797eb5c9006cf534fe7ec0
Eric Robert <eric@datacratic.com>
2013-07-03 13:44:37 -0400

    rubicon exchange connector now using the openrtb exchange connector

--- plugins/exchange/rubicon_exchange_connector.cc
+++ plugins/exchange/rubicon_exchange_connector.cc
@@ -21,64 +21,7 @@
 using namespace std;
 using namespace Datacratic;
 
-namespace Datacratic {
-
-template<typename T, int I, typename S>
-Json::Value jsonEncode(const ML::compact_vector<T, I, S> & vec)
-{
-    Json::Value result(Json::arrayValue);
-    for (unsigned i = 0;  i < vec.size();  ++i)
-        result[i] = jsonEncode(vec[i]);
-    return result;
-}
-
-template<typename T, int I, typename S>
-ML::compact_vector<T, I, S>
-jsonDecode(const Json::Value & val, ML::compact_vector<T, I, S> *)
-{
-    ExcAssert(val.isArray());
-    ML::compact_vector<T, I, S> res;
-    res.reserve(val.size());
-    for (unsigned i = 0;  i < val.size();  ++i)
-        res.push_back(jsonDecode(val[i], (T*)0));
-    return res;
-}
-
-
-} // namespace Datacratic
-
-namespace OpenRTB {
-
-template<typename T>
-Json::Value jsonEncode(const OpenRTB::List<T> & vec)
-{
-    using Datacratic::jsonEncode;
-    Json::Value result(Json::arrayValue);
-    for (unsigned i = 0;  i < vec.size();  ++i)
-        result[i] = jsonEncode(vec[i]);
-    return result;
-}
-
-template<typename T>
-OpenRTB::List<T>
-jsonDecode(const Json::Value & val, OpenRTB::List<T> *)
-{
-    using Datacratic::jsonDecode;
-    ExcAssert(val.isArray());
-    OpenRTB::List<T> res;
-    res.reserve(val.size());
-    for (unsigned i = 0;  i < val.size();  ++i)
-        res.push_back(jsonDecode(val[i], (T*)0));
-    return res;
-}
-
-} // namespace OpenRTB
-
 namespace RTBKIT {
-
-BOOST_STATIC_ASSERT(hasFromJson<Datacratic::Id>::value == true);
-BOOST_STATIC_ASSERT(hasFromJson<int>::value == false);
-
 
 /*****************************************************************************/
 /* RUBICON EXCHANGE CONNECTOR                                                */
@@ -86,177 +29,19 @@ BOOST_STATIC_ASSERT(hasFromJson<int>::value == false);
 
 RubiconExchangeConnector::
 RubiconExchangeConnector(ServiceBase & owner, const std::string & name)
-    : HttpExchangeConnector(name, owner)
-{
-}
-
-RubiconExchangeConnector::
-RubiconExchangeConnector(const std::string & name,
-                         std::shared_ptr<ServiceProxies> proxies)
-    : HttpExchangeConnector(name, proxies)
+    : OpenRTBExchangeConnector(owner, name)
 {
     this->auctionResource = "/auctions";
     this->auctionVerb = "POST";
 }
 
-std::shared_ptr<BidRequest>
 RubiconExchangeConnector::
-parseBidRequest(HttpAuctionHandler & connection,
-                const HttpHeader & header,
-                const std::string & payload)
+RubiconExchangeConnector(const std::string & name,
+                         std::shared_ptr<ServiceProxies> proxies)
+    : OpenRTBExchangeConnector(name, proxies)
 {
-    std::shared_ptr<BidRequest> res;
-
-    // Check for JSON content-type
-    if (header.contentType != "application/json") {
-        connection.sendErrorResponse("non-JSON request");
-        return res;
-    }
-
-    // Check for the x-openrtb-version header
-    auto it = header.headers.find("x-openrtb-version");
-    if (it == header.headers.end()) {
-        connection.sendErrorResponse("no OpenRTB version header supplied");
-        return res;
-    }
-
-    // Check that it's version 2.1
-    string openRtbVersion = it->second;
-    if (openRtbVersion != "2.1") {
-        connection.sendErrorResponse("expected OpenRTB version 2.1; got " + openRtbVersion);
-        return res;
-    }
-
-    // Parse the bid request
-    ML::Parse_Context context("Bid Request", payload.c_str(), payload.size());
-    res.reset(OpenRtbBidRequestParser::parseBidRequest(context, exchangeName(), exchangeName()));
-        
-    cerr << res->toJson() << endl;
-
-    return res;
-}
-
-double
-RubiconExchangeConnector::
-getTimeAvailableMs(HttpAuctionHandler & connection,
-                   const HttpHeader & header,
-                   const std::string & payload)
-{
-    // Scan the payload quickly for the tmax parameter.
-    static const string toFind = "\"tmax\":";
-    string::size_type pos = payload.find(toFind);
-    if (pos == string::npos)
-        return 10.0;
-        
-    int tmax = atoi(payload.c_str() + pos + toFind.length());
-        
-    return tmax;
-}
-
-HttpResponse
-RubiconExchangeConnector::
-getResponse(const HttpAuctionHandler & connection,
-            const HttpHeader & requestHeader,
-            const Auction & auction) const
-{
-    const Auction::Data * current = auction.getCurrentData();
-
-    if (current->hasError())
-        return getErrorResponse(connection, auction, current->error + ": " + current->details);
-    
-    OpenRTB::BidResponse response;
-    response.id = auction.id;
-
-    std::map<Id, int> seatToBid;
-
-    string en = exchangeName();
-
-    // Create a spot for each of the bid responses
-    for (unsigned spotNum = 0; spotNum < current->responses.size();
-         ++spotNum) {
-        if (!current->hasValidResponse(spotNum))
-            continue;
-        
-        // Get the winning bid
-        auto & resp = current->winningResponse(spotNum);
-
-        // Find how the agent is configured.  We need to copy some of the
-        // fields into the bid.
-        const AgentConfig * config
-            = std::static_pointer_cast<const AgentConfig>
-            (resp.agentConfig).get();
-
-        // Get the exchange specific data for this campaign
-        auto cpinfo = config->getProviderData<CampaignInfo>(en);
-
-        // Put in the fixed parts from the creative
-        int creativeIndex = resp.agentCreativeIndex;
-
-        auto & creative = config->creatives.at(creativeIndex);
-
-        // Get the exchange specific data for this creative
-        auto crinfo = creative.getProviderData<CreativeInfo>(en);
-        
-        // Find the index in the seats array
-        int seatIndex = -1;
-        {
-            auto it = seatToBid.find(cpinfo->seat);
-            if (it == seatToBid.end()) {
-                seatIndex = seatToBid.size();
-                seatToBid[cpinfo->seat] = seatIndex;
-                response.seatbid.emplace_back();
-                response.seatbid.back().seat = cpinfo->seat;
-            }
-            else seatIndex = it->second;
-        }
-        
-        // Get the seatBid object
-        OpenRTB::SeatBid & seatBid = response.seatbid.at(seatIndex);
-        
-        // Add a new bid to the array
-        seatBid.bid.emplace_back();
-        auto & b = seatBid.bid.back();
-        
-        // Put in the variable parts
-        b.cid = Id(resp.agent);
-        b.id = Id(auction.id, auction.request->imp[0].id);
-        b.impid = auction.request->imp[spotNum].id;
-        b.price.val = USD_CPM(resp.price.maxPrice);
-        b.adm = crinfo->adm;
-        b.adomain = crinfo->adomain;
-        b.crid = crinfo->crid;
-    }
-
-    if (seatToBid.empty())
-        return HttpResponse(204, "none", "");
-
-    static Datacratic::DefaultDescription<OpenRTB::BidResponse> desc;
-    std::ostringstream stream;
-    StreamJsonPrintingContext context(stream);
-    desc.printJsonTyped(&response, context);
-
-    cerr << Json::parse(stream.str());
-
-    return HttpResponse(200, "application/json", stream.str());
-}
-
-HttpResponse
-RubiconExchangeConnector::
-getDroppedAuctionResponse(const HttpAuctionHandler & connection,
-                          const std::string & reason) const
-{
-    return HttpResponse(204, "application/json", "{}");
-}
-
-HttpResponse
-RubiconExchangeConnector::
-getErrorResponse(const HttpAuctionHandler & connection,
-                 const Auction & auction,
-                 const std::string & errorMessage) const
-{
-    Json::Value response;
-    response["error"] = errorMessage;
-    return HttpResponse(400, response);
+    this->auctionResource = "/auctions";
+    this->auctionVerb = "POST";
 }
 
 ExchangeConnector::ExchangeCompatibility
@@ -408,6 +193,65 @@ decodeWinPrice(const std::string & sharedSecret,
     float res = boost::lexical_cast<float>(recovered);
 
     return res;
+}
+
+void
+RubiconExchangeConnector::
+setSeatBid(Auction const & auction,
+           int spotNum,
+           OpenRTB::BidResponse & response) const
+{
+    const Auction::Data * current = auction.getCurrentData();
+    
+    // Get the winning bid
+    auto & resp = current->winningResponse(spotNum);
+
+    // Find how the agent is configured.  We need to copy some of the
+    // fields into the bid.
+    const AgentConfig * config =
+        std::static_pointer_cast<const AgentConfig>(resp.agentConfig).get();
+
+    std::string en = exchangeName();
+
+    // Get the exchange specific data for this campaign
+    auto cpinfo = config->getProviderData<CampaignInfo>(en);
+
+    // Put in the fixed parts from the creative
+    int creativeIndex = resp.agentCreativeIndex;
+
+    auto & creative = config->creatives.at(creativeIndex);
+
+    // Get the exchange specific data for this creative
+    auto crinfo = creative.getProviderData<CreativeInfo>(en);
+
+    // Find the index in the seats array
+    int seatIndex = 0;
+    while(response.seatbid.size() != seatIndex) {
+        if(response.seatbid[seatIndex].seat == cpinfo->seat) break;
+        ++seatIndex;
+    }
+
+    // Create if required
+    if(seatIndex == response.seatbid.size()) {
+        response.seatbid.emplace_back();
+        response.seatbid.back().seat = cpinfo->seat;
+    }
+
+    // Get the seatBid object
+    OpenRTB::SeatBid & seatBid = response.seatbid.at(seatIndex);
+    
+    // Add a new bid to the array
+    seatBid.bid.emplace_back();
+    auto & b = seatBid.bid.back();
+    
+    // Put in the variable parts
+    b.cid = Id(resp.agent);
+    b.id = Id(auction.id, auction.request->imp[0].id);
+    b.impid = auction.request->imp[spotNum].id;
+    b.price.val = USD_CPM(resp.price.maxPrice);
+    b.adm = crinfo->adm;
+    b.adomain = crinfo->adomain;
+    b.crid = crinfo->crid;
 }
 
 } // namespace RTBKIT
--- plugins/exchange/rubicon_exchange_connector.h
+++ plugins/exchange/rubicon_exchange_connector.h
@@ -6,7 +6,7 @@
 
 #pragma once
 
-#include "rtbkit/plugins/exchange/http_exchange_connector.h"
+#include "rtbkit/plugins/exchange/openrtb_exchange_connector.h"
 
 namespace RTBKIT {
 
@@ -17,12 +17,9 @@ namespace RTBKIT {
 
 /** Exchange connector for Rubicon.  This speaks their flavour of the
     OpenRTB 2.1 protocol.
-
-    Configuration options are the same as the HttpExchangeConnector on which
-    it is based.
 */
 
-struct RubiconExchangeConnector: public HttpExchangeConnector {
+struct RubiconExchangeConnector: public OpenRTBExchangeConnector {
     RubiconExchangeConnector(ServiceBase & owner, const std::string & name);
     RubiconExchangeConnector(const std::string & name,
                              std::shared_ptr<ServiceProxies> proxies);
@@ -34,30 +31,6 @@ struct RubiconExchangeConnector: public HttpExchangeConnector {
     virtual std::string exchangeName() const {
         return exchangeNameString();
     }
-
-    virtual std::shared_ptr<BidRequest>
-    parseBidRequest(HttpAuctionHandler & connection,
-                    const HttpHeader & header,
-                    const std::string & payload);
-
-    virtual double
-    getTimeAvailableMs(HttpAuctionHandler & connection,
-                       const HttpHeader & header,
-                       const std::string & payload);
-
-    virtual HttpResponse
-    getResponse(const HttpAuctionHandler & connection,
-                const HttpHeader & requestHeader,
-                const Auction & auction) const;
-
-    virtual HttpResponse
-    getDroppedAuctionResponse(const HttpAuctionHandler & connection,
-                              const std::string & reason) const;
-
-    virtual HttpResponse
-    getErrorResponse(const HttpAuctionHandler & connection,
-                     const Auction & auction,
-                     const std::string & errorMessage) const;
 
     /** This is the information that the Rubicon exchange needs to keep
         for each campaign (agent).
@@ -74,11 +47,11 @@ struct RubiconExchangeConnector: public HttpExchangeConnector {
         filter and serve a creative.
     */
     struct CreativeInfo {
-        std::string adm;                  ///< Ad markup
-        std::vector<std::string> adomain; ///< Advertiser domains
-        Id crid;                          ///< Creative ID
+        std::string adm;                                ///< Ad markup
+        std::vector<std::string> adomain;               ///< Advertiser domains
+        Id crid;                                        ///< Creative ID
         OpenRTB::List<OpenRTB::CreativeAttribute> attr; ///< Creative attributes
-        std::string ext_creativeapi;      ///< Creative API
+        std::string ext_creativeapi;                    ///< Creative API
     };
 
     virtual ExchangeCompatibility
@@ -88,6 +61,11 @@ struct RubiconExchangeConnector: public HttpExchangeConnector {
     // Rubicon win price decoding function.
     static float decodeWinPrice(const std::string & sharedSecret,
                                 const std::string & winPriceStr);
+
+private:
+    virtual void setSeatBid(Auction const & auction,
+                            int spotNum,
+                            OpenRTB::BidResponse & response) const;
 };
 
 
