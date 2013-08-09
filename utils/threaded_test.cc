ea83551da65950096c34f131d41389747222330f
Datacratic Operations <operations@datacratic.com>
2013-08-09 10:30:27 -0400

    fixed some compilation issues with gcc 4.7

--- utils/threaded_test.cc
+++ utils/threaded_test.cc
@@ -133,7 +133,7 @@ run(unsigned durationMs)
                 auto& future = futures[i][j];
 
                 if (!future.valid()) continue;
-                if (!future.wait_for(chrono::seconds(0))) {
+                if (future.wait_for(chrono::seconds(0)) != std::future_status::ready) {
                     remaining++;
                     continue;
                 }
