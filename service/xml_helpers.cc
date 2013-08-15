208120fca38170039838db541ab18dac0e46dff5
Wolfgang Sourdeau <wolfgang@datacratic.com>
2013-08-15 18:04:12 -0400

    fixed decl for gcc 4.6

--- service/xml_helpers.cc
+++ service/xml_helpers.cc
@@ -1,17 +1,24 @@
-/* xml_helpers.h                                                   -*- C++ -*-
+/* xml_helpers.cc                                                  -*- C++ -*-
    Jeremy Barnes, 12 May 2013
    Copyright (c) 2013 Datacratic Inc.  All rights reserved.
    
    Helper functions to deal with XML.   
 */
 
+#include <memory>
+
+#include "jml/utils/string_functions.h"
+
 #include "xml_helpers.h"
+
+
+using namespace std;
 
 
 namespace Datacratic {
 
 tinyxml2::XMLElement *
-extractNode(tinyxml2::XMLElement * element, const std::string & path)
+extractNode(tinyxml2::XMLElement * element, const string & path)
 {
     using namespace std;
 
