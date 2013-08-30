4001e4816ed29486468d1714298e35c50e491715
Sunil Rottoo <sunil@datacratic.com>
2013-08-30 11:07:22 -0400

    service/testing/multiple_service_test.cc

--- service/testing/multiple_service_test.cc
+++ service/testing/multiple_service_test.cc
@@ -85,123 +85,6 @@ struct EchoService : public ServiceBase {
 
     ZmqNamedClientBus toClients;
 };
-#if 0
-BOOST_AUTO_TEST_CASE( test_early_connection )
-{
-    /** Test that we can do a "connect", then start the service, and
-        have the connection come up once the service appears.
-    */
-    cerr << "Testing early connection..." << endl;
-
-    ZooKeeper::TemporaryServer zookeeper;
-    zookeeper.start();
-
-    auto proxies = std::make_shared<ServiceProxies>();
-    proxies->useZookeeper(ML::format("localhost:%d", zookeeper.getPort()));
-
-    ZmqNamedClientBusProxy connection(proxies->zmqContext);
-    connection.init(proxies->config, "client1");
-    connection.connectHandler = [&] (const std::string & svc) {
-        cerr << "connected to " << svc << endl;
-    };
-
-    connection.disconnectHandler = [&] (const std::string  & svc) {
-        cerr << "disconnected from " << svc << endl;
-    };
-
-    connection.start();
-    connection.connectToServiceClass("echo", "echo");
-
-    //for(int i = 0; i != 2; ++i)
-    {
-        while(connection.isConnected()) {
-            ML::sleep(0.1);
-        }
-
-        BOOST_CHECK_EQUAL(connection.isConnected(), false);
-
-        proxies->config->removePath("");
-
-        EchoService service(proxies, "echo");
-        service.init();
-        auto addr = service.bindTcp();
-        cerr << "echo service is listening on " << addr << endl;
-        service.start();
-
-        //proxies->config->dump(cerr);
-
-        while(!connection.isConnected()) {
-            ML::sleep(0.1);
-        }
-
-        cerr << "Checking that we are connected " << endl;
-        BOOST_CHECK_EQUAL(connection.isConnected(), true);
-    }
-
-    std::cerr << "done." << std::endl;
-}
-
-
-BOOST_AUTO_TEST_CASE( test_multiple_services )
-{
-    ZooKeeper::TemporaryServer zookeeper;
-    zookeeper.start();
-
-    auto proxies = std::make_shared<ServiceProxies>();
-    proxies->useZookeeper(ML::format("localhost:%d", zookeeper.getPort()));
-
-    cerr << "Starting multiple services test " << endl;
-
-    ZmqMultipleNamedClientBusProxy connection(proxies->zmqContext);
-    connection.init(proxies->config, "client1");
-
-    connection.connectHandler = [&] (const std::string & svc)
-        {
-            cerr << "connected to " << svc << endl;
-        };
-
-    connection.disconnectHandler = [&] (const std::string  & svc)
-        {
-            cerr << "disconnected from " << svc << endl;
-        };
-
-    connection.start();
-
-    BOOST_CHECK_EQUAL(connection.connectionCount(), 0);
-
-    connection.connectAllServiceProviders("echo", "echo");
-
-    BOOST_CHECK_EQUAL(connection.connectionCount(), 0);
-
-    std::vector<unique_ptr<EchoService> > services;
-
-    auto startService = [&] ()
-        {
-            services.emplace_back(new EchoService(proxies, "echo" + to_string(services.size())));
-            EchoService & service = *services.back();
-            service.init();
-            auto addr = service.bindTcp();
-            cerr << "echo service is listening on " << addr << endl;
-            service.start();
-        };
-
-    startService();
-
-    proxies->config->dump(cerr);
-
-    ML::sleep(0.1);
-
-    BOOST_CHECK_EQUAL(connection.connectionCount(), 1);
-
-    cerr << "shutting down" << endl;
-
-    connection.shutdown();
-
-    for (unsigned i = 0;  i < services.size();  ++i)
-        services[i]->shutdown();
-
-}
-#endif
 BOOST_AUTO_TEST_CASE( test_service_zk_disconnect )
 {
     ZooKeeper::TemporaryServer zookeeper;
@@ -281,6 +164,121 @@ BOOST_AUTO_TEST_CASE( test_service_zk_disconnect )
 
     for (unsigned i = 0;  i < services.size();  ++i)
         services[i]->shutdown();
-
-    zookeeper.shutdown();
 }
+
+#if 1
+BOOST_AUTO_TEST_CASE( test_early_connection )
+{
+    /** Test that we can do a "connect", then start the service, and
+        have the connection come up once the service appears.
+    */
+    cerr << "Testing early connection..." << endl;
+
+    ZooKeeper::TemporaryServer zookeeper;
+    zookeeper.start();
+
+    auto proxies = std::make_shared<ServiceProxies>();
+    proxies->useZookeeper(ML::format("localhost:%d", zookeeper.getPort()));
+
+    ZmqNamedClientBusProxy connection(proxies->zmqContext);
+    connection.init(proxies->config, "client1");
+    connection.connectHandler = [&] (const std::string & svc) {
+        cerr << "connected to " << svc << endl;
+    };
+
+    connection.disconnectHandler = [&] (const std::string  & svc) {
+        cerr << "disconnected from " << svc << endl;
+    };
+
+    connection.start();
+    connection.connectToServiceClass("echo", "echo");
+
+    //for(int i = 0; i != 2; ++i)
+    {
+        while(connection.isConnected()) {
+            ML::sleep(0.1);
+        }
+
+        BOOST_CHECK_EQUAL(connection.isConnected(), false);
+
+        proxies->config->removePath("");
+
+        EchoService service(proxies, "echo");
+        service.init();
+        auto addr = service.bindTcp();
+        cerr << "echo service is listening on " << addr << endl;
+        service.start();
+
+        //proxies->config->dump(cerr);
+
+        while(!connection.isConnected()) {
+            ML::sleep(0.1);
+        }
+
+        cerr << "Checking that we are connected " << endl;
+        BOOST_CHECK_EQUAL(connection.isConnected(), true);
+    }
+
+    std::cerr << "done." << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE( test_multiple_services )
+{
+    ZooKeeper::TemporaryServer zookeeper;
+    zookeeper.start();
+
+    auto proxies = std::make_shared<ServiceProxies>();
+    proxies->useZookeeper(ML::format("localhost:%d", zookeeper.getPort()));
+
+    cerr << "Starting multiple services test " << endl;
+
+    ZmqMultipleNamedClientBusProxy connection(proxies->zmqContext);
+    connection.init(proxies->config, "client1");
+
+    connection.connectHandler = [&] (const std::string & svc)
+        {
+            cerr << "connected to " << svc << endl;
+        };
+
+    connection.disconnectHandler = [&] (const std::string  & svc)
+        {
+            cerr << "disconnected from " << svc << endl;
+        };
+
+    connection.start();
+
+    BOOST_CHECK_EQUAL(connection.connectionCount(), 0);
+
+    connection.connectAllServiceProviders("echo", "echo");
+
+    BOOST_CHECK_EQUAL(connection.connectionCount(), 0);
+
+    std::vector<unique_ptr<EchoService> > services;
+
+    auto startService = [&] ()
+        {
+            services.emplace_back(new EchoService(proxies, "echo" + to_string(services.size())));
+            EchoService & service = *services.back();
+            service.init();
+            auto addr = service.bindTcp();
+            cerr << "echo service is listening on " << addr << endl;
+            service.start();
+        };
+
+    startService();
+
+    proxies->config->dump(cerr);
+
+    ML::sleep(0.1);
+
+    BOOST_CHECK_EQUAL(connection.connectionCount(), 1);
+
+    cerr << "shutting down" << endl;
+
+    connection.shutdown();
+
+    for (unsigned i = 0;  i < services.size();  ++i)
+        services[i]->shutdown();
+
+}
+#endif
