a1a597f88ac742afff090229407e3a9420e5b72d
Wolfgang Sourdeau <wolfgang@datacratic.com>
2013-09-16 09:22:55 -0400

    fixed buffer handling

--- service/testing/runner_test_helper.cc
+++ service/testing/runner_test_helper.cc
@@ -8,17 +8,19 @@ void
 DoOutput(FILE * in, FILE * out)
 {
     int len(0);
-    char buffer[256];
+    char *buffer;
 
     size_t n = ::fread(&len, sizeof(len), 1, in);
     ExcCheckNotEqual(n, 0, "sizeof(len) must be 4");
 
+    buffer = ::malloc(len + 1);
     n = ::fread(&buffer, sizeof(char), len, in);
     ExcCheckNotEqual(n, 0, "received 0 bytes");
 
     buffer[len] = 0;
 
-    fprintf(out, "%s\n", buffer);
+    ::fprintf(out, "%s\n", buffer);
+    ::free(buffer);
 }
 
 void
