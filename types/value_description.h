ac05b5d46c1ff0fd075d0486b379ccca51b0e8a9
Jeremy Barnes <jeremy@datacratic.com>
2013-09-17 21:42:57 -0400

    made map value description more general with helper functions for key stringify/unstringify

--- types/value_description.h
+++ types/value_description.h
@@ -1006,14 +1006,17 @@ struct DefaultDescription<std::set<T> >
     }
 };
 
+inline std::string stringToKey(const std::string & str, std::string *) { return str; }
+inline std::string keyToString(const std::string & str) { return str; }
+
 
 /*****************************************************************************/
 /* DEFAULT DESCRIPTION FOR MAP                                               */
 /*****************************************************************************/
 
-template<typename T>
-struct DefaultDescription<std::map<std::string, T> >
-    : public ValueDescriptionI<std::map<std::string, T>, ValueKind::MAP> {
+template<typename K, typename T>
+struct DefaultDescription<std::map<K, T> >
+    : public ValueDescriptionI<std::map<K, T>, ValueKind::MAP> {
 
     DefaultDescription(ValueDescriptionT<T> * inner
                       = getDefaultDescription((T *)0))
@@ -1027,17 +1030,17 @@ struct DefaultDescription<std::map<std::string, T> >
 
     virtual void parseJson(void * val, JsonParsingContext & context) const
     {
-        auto * val2 = reinterpret_cast<std::map<std::string, T> *>(val);
+        auto * val2 = reinterpret_cast<std::map<K, T> *>(val);
         return parseJsonTyped(val2, context);
     }
 
-    virtual void parseJsonTyped(std::map<std::string, T> * val, JsonParsingContext & context) const
+    virtual void parseJsonTyped(std::map<K, T> * val, JsonParsingContext & context) const
     {
-        std::map<std::string, T> res;
+        std::map<K, T> res;
 
         auto onMember = [&] ()
             {
-                inner->parseJsonTyped(&res[context.fieldName()], context);
+                inner->parseJsonTyped(&res[stringToKey(context.fieldName(), (K *)0)], context);
             };
 
         context.forEachMember(onMember);
@@ -1047,16 +1050,16 @@ struct DefaultDescription<std::map<std::string, T> >
 
     virtual void printJson(const void * val, JsonPrintingContext & context) const
     {
-        auto * val2 = reinterpret_cast<const std::map<std::string, T> *>(val);
+        auto * val2 = reinterpret_cast<const std::map<K, T> *>(val);
         return printJsonTyped(val2, context);
     }
 
-    virtual void printJsonTyped(const std::map<std::string, T> * val,
+    virtual void printJsonTyped(const std::map<K, T> * val,
                                 JsonPrintingContext & context) const
     {
         context.startObject();
         for (auto & v: *val) {
-            context.startMember(v.first);
+            context.startMember(keyToString(v.first));
             inner->printJsonTyped(&v.second, context);
         }
         context.endObject();
@@ -1064,18 +1067,18 @@ struct DefaultDescription<std::map<std::string, T> >
 
     virtual bool isDefault(const void * val) const
     {
-        auto * val2 = reinterpret_cast<const std::map<std::string, T> *>(val);
+        auto * val2 = reinterpret_cast<const std::map<K, T> *>(val);
         return isDefaultTyped(val2);
     }
 
-    virtual bool isDefaultTyped(const std::map<std::string, T> * val) const
+    virtual bool isDefaultTyped(const std::map<K, T> * val) const
     {
         return val->empty();
     }
 
     virtual size_t getFieldCount(const void * val) const
     {
-        auto * val2 = reinterpret_cast<const std::map<std::string, T> *>(val);
+        auto * val2 = reinterpret_cast<const std::map<K, T> *>(val);
         return val2->size();
     }
 
