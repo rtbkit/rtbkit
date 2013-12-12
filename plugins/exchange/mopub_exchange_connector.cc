622b0477a2c66ceceea4e53aa7a0ac5091fcc4a0
modality <modality@gmail.com>
2013-12-12 08:38:46 -0500

    Intial drop for MoPub Exchange Connetor. * Mostly relies on OpenRTB2.1 * Checked against MoPub's sandbox. * TODO: check the many assumptions:   - rtt <= 200ms   - optional fields of BidResponse made mandatory by MoPub (e.g. iurl) * TODO: customer integration/validation

---
+++ plugins/exchange/mopub_exchange_connector.cc
@@ -0,0 +1,331 @@
+/* mopub_exchange_connector.cc
+   Jeremy Barnes, 15 March 2013
+
+   Implementation of the MoPub exchange connector.
+*/
+
+#include "mopub_exchange_connector.h"
+#include "rtbkit/plugins/bid_request/openrtb_bid_request.h"
+#include "rtbkit/plugins/exchange/http_auction_handler.h"
+#include "rtbkit/core/agent_configuration/agent_config.h"
+#include "openrtb/openrtb_parsing.h"
+#include "soa/types/json_printing.h"
+#include <boost/any.hpp>
+#include <boost/lexical_cast.hpp>
+#include "jml/utils/file_functions.h"
+
+#include "crypto++/blowfish.h"
+#include "crypto++/modes.h"
+#include "crypto++/filters.h"
+
+using namespace std;
+using namespace Datacratic;
+
+namespace RTBKIT {
+
+/*****************************************************************************/
+/* MOPUB EXCHANGE CONNECTOR                                                */
+/*****************************************************************************/
+
+MoPubExchangeConnector::
+MoPubExchangeConnector(ServiceBase & owner, const std::string & name)
+    : OpenRTBExchangeConnector(owner, name) {
+    this->auctionResource = "/auctions";
+    this->auctionVerb = "POST";
+}
+
+MoPubExchangeConnector::
+MoPubExchangeConnector(const std::string & name,
+                       std::shared_ptr<ServiceProxies> proxies)
+    : OpenRTBExchangeConnector(name, proxies) {
+    this->auctionResource = "/auctions";
+    this->auctionVerb = "POST";
+}
+
+ExchangeConnector::ExchangeCompatibility
+MoPubExchangeConnector::
+getCampaignCompatibility(const AgentConfig & config,
+                         bool includeReasons) const {
+    ExchangeCompatibility result;
+    result.setCompatible();
+
+    auto cpinfo = std::make_shared<CampaignInfo>();
+
+    const Json::Value & pconf = config.providerConfig["mopub"];
+
+    try {
+        cpinfo->seat = Id(pconf["seat"].asString());
+        if (!cpinfo->seat)
+            result.setIncompatible("providerConfig.mopub.seat is null",
+                                   includeReasons);
+    } catch (const std::exception & exc) {
+        result.setIncompatible
+        (string("providerConfig.mopub.seat parsing error: ")
+         + exc.what(), includeReasons);
+        return result;
+    }
+
+    try {
+        cpinfo->iurl = pconf["iurl"].asString();
+        if (!cpinfo->iurl.size())
+            result.setIncompatible("providerConfig.mopub.iurl is null",
+                                   includeReasons);
+    } catch (const std::exception & exc) {
+        result.setIncompatible
+        (string("providerConfig.mopub.iurl parsing error: ")
+         + exc.what(), includeReasons);
+        return result;
+    }
+
+    result.info = cpinfo;
+
+    return result;
+}
+
+namespace {
+
+using Datacratic::jsonDecode;
+
+/** Given a configuration field, convert it to the appropriate JSON */
+template<typename T>
+void getAttr(ExchangeConnector::ExchangeCompatibility & result,
+             const Json::Value & config,
+             const char * fieldName,
+             T & field,
+             bool includeReasons) {
+    try {
+        if (!config.isMember(fieldName)) {
+            result.setIncompatible
+            ("creative[].providerConfig.mopub." + string(fieldName)
+             + " must be specified", includeReasons);
+            return;
+        }
+
+        const Json::Value & val = config[fieldName];
+
+        jsonDecode(val, field);
+    } catch (const std::exception & exc) {
+        result.setIncompatible("creative[].providerConfig.mopub."
+                               + string(fieldName) + ": error parsing field: "
+                               + exc.what(), includeReasons);
+        return;
+    }
+}
+
+} // file scope
+
+ExchangeConnector::ExchangeCompatibility
+MoPubExchangeConnector::
+getCreativeCompatibility(const Creative & creative,
+                         bool includeReasons) const {
+    ExchangeCompatibility result;
+    result.setCompatible();
+
+    auto crinfo = std::make_shared<CreativeInfo>();
+
+    const Json::Value & pconf = creative.providerConfig["mopub"];
+
+    // 1.  Must have mopub.attr containing creative attributes.  These
+    //     turn into MoPubCreativeAttribute filters.
+    getAttr(result, pconf, "attr", crinfo->attr, includeReasons);
+
+    // TODO: create filter from these...
+
+    // 2.  Must have mopub.adm that includes MoPub's macro
+    getAttr(result, pconf, "adm", crinfo->adm, includeReasons);
+    if (crinfo->adm.find("${AUCTION_PRICE:BF}") == string::npos)
+        result.setIncompatible
+        ("creative[].providerConfig.mopub.adm ad markup must contain "
+         "encrypted win price macro ${AUCTION_PRICE:BF}",
+         includeReasons);
+
+    // 3.  Must have creative ID in mopub.crid
+    getAttr(result, pconf, "crid", crinfo->crid, includeReasons);
+    if (!crinfo->crid)
+        result.setIncompatible
+        ("creative[].providerConfig.mopub.crid is null",
+         includeReasons);
+
+    // 4.  Must have advertiser names array in mopub.adomain
+    getAttr(result, pconf, "adomain", crinfo->adomain,  includeReasons);
+    if (crinfo->adomain.empty())
+        result.setIncompatible
+        ("creative[].providerConfig.mopub.adomain is empty",
+         includeReasons);
+
+    // Cache the information
+    result.info = crinfo;
+
+    return result;
+}
+
+float
+MoPubExchangeConnector::
+decodeWinPrice(const std::string & sharedSecret,
+               const std::string & winPriceStr) {
+    ExcAssertEqual(winPriceStr.length(), 16);
+
+    auto tox = [] (char c) {
+        if (c >= '0' && c <= '9')
+            return c - '0';
+        else if (c >= 'A' && c <= 'F')
+            return 10 + c - 'A';
+        else if (c >= 'a' && c <= 'f')
+            return 10 + c - 'a';
+        throw ML::Exception("invalid hex digit");
+    };
+
+    unsigned char input[8];
+    for (unsigned i = 0;  i < 8;  ++i)
+        input[i]
+            = tox(winPriceStr[i * 2]) * 16
+              + tox(winPriceStr[i * 2 + 1]);
+
+    CryptoPP::ECB_Mode<CryptoPP::Blowfish>::Decryption d;
+    d.SetKey((byte *)sharedSecret.c_str(), sharedSecret.size());
+    CryptoPP::StreamTransformationFilter
+    filt(d, nullptr,
+         CryptoPP::StreamTransformationFilter::NO_PADDING);
+    filt.Put(input, 8);
+    filt.MessageEnd();
+    char recovered[9];
+    size_t nrecovered = filt.Get((byte *)recovered, 8);
+
+    ExcAssertEqual(nrecovered, 8);
+    recovered[nrecovered] = 0;
+
+    float res = boost::lexical_cast<float>(recovered);
+
+    return res;
+}
+
+std::shared_ptr<BidRequest>
+MoPubExchangeConnector::
+parseBidRequest(HttpAuctionHandler & connection,
+                const HttpHeader & header,
+                const std::string & payload)
+{
+    std::shared_ptr<BidRequest> res;
+
+    // Check for JSON content-type
+    if (header.contentType != "application/json") {
+        connection.sendErrorResponse("non-JSON request");
+        return res;
+    }
+
+    // Parse the bid request
+    ML::Parse_Context context("Bid Request", payload.c_str(), payload.size());
+    res.reset(OpenRtbBidRequestParser::parseBidRequest(context, exchangeName(), exchangeName()));
+
+    return res;
+}
+
+#if 0
+HttpResponse
+MoPubExchangeConnector::
+getResponse(const HttpAuctionHandler & connection,
+            const HttpHeader & requestHeader,
+            const Auction & auction) const
+{
+    const Auction::Data * current = auction.getCurrentData();
+
+    if (current->hasError())
+        return getErrorResponse(connection, auction,
+                                current->error + ": " + current->details);
+
+    OpenRTB::BidResponse response;
+    response.id = auction.id;
+
+    response.ext = getResponseExt(connection, auction);
+
+    // Create a spot for each of the bid responses
+    for (unsigned spotNum = 0; spotNum < current->responses.size(); ++spotNum) {
+        if (!current->hasValidResponse(spotNum))
+            continue;
+
+        setSeatBid(auction, spotNum, response);
+    }
+
+    if (response.seatbid.empty())
+        return HttpResponse(204, "none", "");
+
+    static Datacratic::DefaultDescription<OpenRTB::BidResponse> desc;
+    std::ostringstream stream;
+    StreamJsonPrintingContext context(stream);
+    desc.printJsonTyped(&response, context);
+
+    return HttpResponse(200, "application/json", stream.str());
+}
+#endif
+void
+MoPubExchangeConnector::
+setSeatBid(Auction const & auction,
+           int spotNum,
+           OpenRTB::BidResponse & response) const {
+
+    const Auction::Data * current = auction.getCurrentData();
+
+    // Get the winning bid
+    auto & resp = current->winningResponse(spotNum);
+
+    // Find how the agent is configured.  We need to copy some of the
+    // fields into the bid.
+    const AgentConfig * config =
+        std::static_pointer_cast<const AgentConfig>(resp.agentConfig).get();
+
+    std::string en = exchangeName();
+
+    // Get the exchange specific data for this campaign
+    auto cpinfo = config->getProviderData<CampaignInfo>(en);
+
+    // Put in the fixed parts from the creative
+    int creativeIndex = resp.agentCreativeIndex;
+
+    auto & creative = config->creatives.at(creativeIndex);
+
+    // Get the exchange specific data for this creative
+    auto crinfo = creative.getProviderData<CreativeInfo>(en);
+
+    // Find the index in the seats array
+    int seatIndex = 0;
+    while(response.seatbid.size() != seatIndex) {
+        if(response.seatbid[seatIndex].seat == cpinfo->seat) break;
+        ++seatIndex;
+    }
+
+    // Create if required
+    if(seatIndex == response.seatbid.size()) {
+        response.seatbid.emplace_back();
+        response.seatbid.back().seat = cpinfo->seat;
+    }
+
+    // Get the seatBid object
+    OpenRTB::SeatBid & seatBid = response.seatbid.at(seatIndex);
+
+    // Add a new bid to the array
+    seatBid.bid.emplace_back();
+    auto & b = seatBid.bid.back();
+
+    // Put in the variable parts
+    b.cid = Id(resp.agent);
+    b.id = Id(auction.id, auction.request->imp[0].id);
+    b.impid = auction.request->imp[spotNum].id;
+    b.price.val = USD_CPM(resp.price.maxPrice);
+    b.adm = crinfo->adm;
+    b.adomain = crinfo->adomain;
+    b.crid = crinfo->crid;
+    b.iurl = cpinfo->iurl;
+}
+
+} // namespace RTBKIT
+
+namespace {
+using namespace RTBKIT;
+
+struct Init {
+    Init() {
+        ExchangeConnector::registerFactory<MoPubExchangeConnector>();
+    }
+} init;
+}
+
