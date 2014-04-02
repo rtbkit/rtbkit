3334ef05cb9659d8cb2213e103ecfbd2b25b7b5e
Jeremy Barnes <jeremy@datacratic.com>
2014-04-02 12:26:18 -0400

    support OPTIONS calls for RestRequestRouter

--- service/rest_request_router.cc
+++ service/rest_request_router.cc
@@ -98,6 +98,18 @@ handleRequest(const RestServiceEndpoint::ConnectionId & connection,
     }
 }
 
+static std::string getVerbsStr(const std::set<std::string> & verbs)
+{
+    string verbsStr;
+    for (auto v: verbs) {
+        if (!verbsStr.empty())
+            verbsStr += ",";
+        verbsStr += v;
+    }
+            
+    return verbsStr;
+}
+
 RestRequestRouter::
 MatchResult
 RestRequestRouter::
@@ -114,9 +126,26 @@ processRequest(const RestServiceEndpoint::ConnectionId & connection,
              << " with " << subRoutes.size() << " subroutes" << endl;
     }
 
+    if (request.verb == "OPTIONS") {
+        Json::Value help;
+        std::set<std::string> verbs;
+
+        this->options(verbs, help, request, context);
+
+        RestParams headers = { { "Allow", getVerbsStr(verbs) } };
+        
+        if (verbs.empty())
+            connection.sendHttpResponse(400, "", "", headers);
+        else
+            connection.sendHttpResponse(200, help.toStyledString(),
+                                        "application/json",
+                                        headers);
+        return MR_YES;
+    }
+
     if (rootHandler && (!terminal || context.remaining.empty()))
         return rootHandler(connection, request, context);
-        
+
     for (auto & sr: subRoutes) {
         if (debug)
             cerr << "  trying subroute " << sr.router->description << endl;
@@ -136,6 +165,61 @@ processRequest(const RestServiceEndpoint::ConnectionId & connection,
     return MR_NO;
     //connection.sendErrorResponse(404, "invalid route for "
     //                             + request.resource);
+}
+
+void
+RestRequestRouter::
+options(std::set<std::string> & verbsAccepted,
+        Json::Value & help,
+        const RestRequest & request,
+        RestRequestParsingContext & context) const
+{
+    for (auto & sr: subRoutes) {
+        sr.options(verbsAccepted, help, request, context);
+    }
+}
+
+bool
+RestRequestRouter::Route::
+matchPath(const RestRequest & request,
+          RestRequestParsingContext & context) const
+{
+    switch (path.type) {
+    case PathSpec::STRING: {
+        std::string::size_type pos = context.remaining.find(path.path);
+        if (pos == 0) {
+            using namespace std;
+            //cerr << "context string " << pos << endl;
+            context.resources.push_back(path.path);
+            context.remaining = string(context.remaining, path.path.size());
+            break;
+        }
+        else return false;
+    }
+    case PathSpec::REGEX: {
+        boost::smatch results;
+        bool found
+            = boost::regex_search(context.remaining,
+                                  results,
+                                  path.rex)
+            && !results.prefix().matched;  // matches from the start
+        
+        //cerr << "matching regex " << path.path << " against "
+        //     << context.remaining << " with found " << found << endl;
+        if (!found)
+            return false;
+        for (unsigned i = 0;  i < results.size();  ++i)
+            context.resources.push_back(results[i]);
+        context.remaining = std::string(context.remaining,
+                                        results[0].length());
+        break;
+    }
+    case PathSpec::NONE:
+    default:
+        throw ML::Exception("unknown rest request type");
+    }
+
+    return true;
 }
 
 RestRequestRouter::MatchResult
@@ -159,45 +243,37 @@ process(const RestRequest & request,
     // At the end, make sure we put the context back to how it was
     RestRequestParsingContext::StateGuard guard(&context);
 
-    switch (path.type) {
-    case PathSpec::STRING: {
-        std::string::size_type pos = context.remaining.find(path.path);
-        if (pos == 0) {
-            using namespace std;
-            //cerr << "context string " << pos << endl;
-            context.resources.push_back(path.path);
-            context.remaining = string(context.remaining, path.path.size());
-            break;
-        }
-        else return MR_NO;
-    }
-    case PathSpec::REGEX: {
-        boost::smatch results;
-        bool found
-            = boost::regex_search(context.remaining,
-                                  results,
-                                  path.rex)
-            && !results.prefix().matched;  // matches from the start
-        
-        //cerr << "matching regex " << path.path << " against "
-        //     << context.remaining << " with found " << found << endl;
-        if (!found)
-            return MR_NO;
-        for (unsigned i = 0;  i < results.size();  ++i)
-            context.resources.push_back(results[i]);
-        context.remaining = std::string(context.remaining,
-                                        results[0].length());
-        break;
-    }
-    case PathSpec::NONE:
-    default:
-        throw ML::Exception("unknown rest request type");
-    }
-    
+    if (!matchPath(request, context))
+        return MR_NO;
+
     if (extractObject)
         extractObject(context);
 
     return router->processRequest(connection, request, context);
+}
+
+void
+RestRequestRouter::Route::
+options(std::set<std::string> & verbsAccepted,
+        Json::Value & help,
+        const RestRequest & request,
+        RestRequestParsingContext & context) const
+{
+    RestRequestParsingContext::StateGuard guard(&context);
+
+    if (!matchPath(request, context))
+        return;
+
+    if (context.remaining.empty()) {
+        verbsAccepted.insert(filter.verbs.begin(), filter.verbs.end());
+
+        string path = "";//this->path.getPathDesc();
+        Json::Value & sri = help[path + getVerbsStr(filter.verbs)];
+        this->path.getHelp(sri);
+        filter.getHelp(sri);
+        router->getHelp(help, path, filter.verbs);
+    }
+    router->options(verbsAccepted, help, request, context);
 }
 
 void
@@ -254,22 +330,10 @@ addHelpRoute(PathSpec path, RequestFilter filter)
 void
 RestRequestRouter::
 getHelp(Json::Value & result, const std::string & currentPath,
-        const std::set<std::string> & verbs)
+        const std::set<std::string> & verbs) const
 {
-    auto getVerbsStr = [] (const std::set<std::string> & verbs)
-        {
-            string verbsStr;
-            for (auto v: verbs) {
-                if (!verbsStr.empty())
-                    verbsStr += ",";
-                else verbsStr += " ";
-                verbsStr += v;
-            }
-            
-            return verbsStr;
-        };
-
-    Json::Value & v = result[currentPath + getVerbsStr(verbs)];
+    Json::Value & v = result[(currentPath.empty() ? "" : currentPath + " ")
+                             + getVerbsStr(verbs)];
 
     v["description"] = description;
     if (!argHelp.isNull())
@@ -277,7 +341,8 @@ getHelp(Json::Value & result, const std::string & currentPath,
     
     for (unsigned i = 0;  i < subRoutes.size();  ++i) {
         string path = currentPath + subRoutes[i].path.getPathDesc();
-        Json::Value & sri = result[path + getVerbsStr(subRoutes[i].filter.verbs)];
+        Json::Value & sri = result[(path.empty() ? "" : path + " ")
+                                   + getVerbsStr(subRoutes[i].filter.verbs)];
         subRoutes[i].path.getHelp(sri);
         subRoutes[i].filter.getHelp(sri);
         subRoutes[i].router->getHelp(result, path, subRoutes[i].filter.verbs);
