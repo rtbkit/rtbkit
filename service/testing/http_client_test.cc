6a7f375c431f5b7e5909a9d545692ad651850c07
Wolfgang Sourdeau <Wolfgang@Contre.COM>
2014-04-16 17:07:08 -0400

    segregated the test HttpService classes in their own library

--- service/testing/http_client_test.cc
+++ service/testing/http_client_test.cc
@@ -7,156 +7,19 @@
 #include <boost/test/unit_test.hpp>
 
 #include "jml/utils/testing/watchdog.h"
-#include "soa/service/http_endpoint.h"
-#include "soa/service/named_endpoint.h"
 #include "soa/service/message_loop.h"
 #include "soa/service/rest_proxy.h"
-#include "soa/service/rest_service_endpoint.h"
 #include "soa/service/http_client.h"
+
+#include "test_http_services.h"
+
 
 using namespace std;
 using namespace Datacratic;
 
+
 /* helpers functions used in tests */
 namespace {
-
-struct HttpTestConnHandler;
-
-struct HttpService : public ServiceBase, public HttpEndpoint {
-    HttpService(const shared_ptr<ServiceProxies> & proxies)
-        : ServiceBase("http-test-service", proxies),
-          HttpEndpoint("http-test-service-ep"),
-          portToUse(0)
-    {
-    }
-
-    ~HttpService()
-    {
-        shutdown();
-    }
-
-    void start()
-    {
-        init(portToUse, "127.0.0.1", 1);
-    }
-
-    virtual shared_ptr<ConnectionHandler> makeNewHandler();
-    virtual void handleHttpPayload(HttpTestConnHandler & handler,
-                                   const HttpHeader & header,
-                                   const string & payload) = 0;
-
-    int portToUse;
-};
-
-struct HttpTestConnHandler : HttpConnectionHandler {
-    virtual void handleHttpPayload(const HttpHeader & header,
-                                   const string & payload) {
-        HttpService *svc = (HttpService *) httpEndpoint;
-        svc->handleHttpPayload(*this, header, payload);
-    }
-
-    void sendResponse(int code, const string & body, const string & type)
-    {
-        putResponseOnWire(HttpResponse(code, type, body));
-    }
-};
-
-shared_ptr<ConnectionHandler>
-HttpService::
-makeNewHandler()
-{
-    return make_shared<HttpTestConnHandler>();
-}
-
-struct HttpGetService : public HttpService {
-    atomic<int> numReqs;
-
-    HttpGetService(const shared_ptr<ServiceProxies> & proxies)
-        : HttpService(proxies), numReqs(0)
-    {}
-
-    struct TestResponse {
-        TestResponse(int code = 0, const string & body = "")
-            : code_(code), body_(body)
-        {}
-
-        int code_;
-        string body_;
-    };
-
-    void handleHttpPayload(HttpTestConnHandler & handler,
-                           const HttpHeader & header,
-                           const string & payload)
-    {
-        numReqs++;
-        // int localRq = numReqs;
-        // if ((localRq % 100) == 0) {
-        //     ::fprintf(stderr, "srv reqs: %d\n", localRq);
-        // }
-        string key = header.verb + ":" + header.resource;
-        if (header.resource == "/timeout") {
-            sleep(3);
-            handler.sendResponse(200, "Will time out", "text/plain");
-        }
-        else if (header.resource == "/headers") {
-            string headersBody("{\n");
-            bool first(true);
-            for (const auto & it: header.headers) {
-                if (first) {
-                    first = false;
-                }
-                else {
-                    headersBody += ",\n";
-                }
-                headersBody += "  \"" + it.first + "\": \"" + it.second + "\"\n";
-            }
-            headersBody += "}\n";
-            handler.sendResponse(200, headersBody, "application/json");
-        }
-        else {
-            const auto & it = responses_.find(key);
-            if (it == responses_.end()) {
-                handler.sendResponse(404, "Not found", "text/plain");
-            }
-            else {
-                const TestResponse & resp = it->second;
-                handler.sendResponse(resp.code_, resp.body_, "text/plain");
-            }
-        }
-    }
-
-    void addResponse(const string & verb, const string & resource,
-                     int code, const string & body)
-    {
-        string key = verb + ":" + resource;
-        responses_[key] = TestResponse(code, body);
-    }
-
-    map<string, TestResponse> responses_;
-};
-
-struct HttpUploadService : public HttpService {
-    HttpUploadService(const shared_ptr<ServiceProxies> & proxies)
-        : HttpService(proxies)
-    {}
-
-    void handleHttpPayload(HttpTestConnHandler & handler,
-                           const HttpHeader & header,
-                           const string & payload)
-    {
-        Json::Value response;
-
-        string cType = header.contentType;
-        if (cType.empty()) {
-            cType = header.tryGetHeader("Content-Type");
-        }
-        response["verb"] = header.verb;
-        response["type"] = cType;
-        response["payload"] = payload;
-        
-        handler.sendResponse(200, response.toString(), "application/json");
-    }
-};
 
 typedef tuple<HttpClientError, int, string> ClientResponse;
 
