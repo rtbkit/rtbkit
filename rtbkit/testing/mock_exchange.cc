1446f843b43ae2f5b66954f682f24b250048d3d0
Rémi Attab <remi@datacratic.com>
2014-04-23 17:02:00 -0400

    Fixed some of the graphite logging for the mock exchange.

--- rtbkit/testing/mock_exchange.cc
+++ rtbkit/testing/mock_exchange.cc
@@ -118,20 +118,24 @@ MockExchange::Worker::bid() {
         auto response = bids->receiveBid();
         exchange->recordHit("responses");
 
-        if (!response.first) continue;
         vector<ExchangeSource::Bid> items = response.second;
+
+        if (!response.first || items.empty()) {
+            exchange->recordHit("noBids");
+            continue;
+        }
 
         for (auto & bid : items) {
             if(bid.maxPrice == 0) continue;
-            exchange->recordHit("responses");
+            exchange->recordHit("bids");
 
             if (!wins) break;
 
             auto ret = isWin(br, bid);
             if (!ret.first) continue;
-			ML::sleep(0.5);
+            ML::sleep(0.5);
 
-			bid.bidTimestamp = Date::now();
+            bid.bidTimestamp = Date::now();
 
             wins->sendWin(br, bid, ret.second);
             exchange->recordHit("wins");
