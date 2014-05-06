5d9dce30a9f6ac77d29c5c7c972d00b1448b7b81
Rémi Attab <remi@datacratic.com>
2014-05-06 14:01:40 -0400

    Log categories are now properly constructed.

--- service/testing/service_testing.mk
+++ service/testing/service_testing.mk
@@ -44,3 +44,5 @@ $(eval $(call library,test_services,test_http_services.cc,services))
 
 $(eval $(call test,http_client_test,services test_services,boost))
 $(eval $(call test,http_client_bench,boost_program_options services test_services,boost manual))
+
+$(eval $(call test,logs_test,services,boost))
