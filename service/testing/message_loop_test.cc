b3c2ec06dbfeb9ce831dba4e532ec4495b2d678b
Wolfgang Sourdeau <Wolfgang@Contre.COM>
2014-05-06 23:19:59 -0400

    removed bogus test

--- service/testing/message_loop_test.cc
+++ service/testing/message_loop_test.cc
@@ -14,34 +14,6 @@
 using namespace std;
 using namespace Datacratic;
 
-/* this test causes a crash because sink is destroyed before
- * MessageLoop::shutdown is invoked */
-BOOST_AUTO_TEST_CASE( test_destruction_order )
-{
-    bool doCrash(false);
-    MessageLoop loop;
-    TypedMessageSink<string> sink(12);
-
-    loop.addSource("sink", sink);
-    loop.start();
-
-    string result;
-    auto onEvent = [&] (string && msg) {
-        cerr << "received and sleeping\n";
-        ML::sleep(5.0);
-        result += msg;
-        cerr << "done sleeping\n";
-    };
-    sink.onEvent = onEvent;
-
-    cerr << "sending msg 1\n";
-    sink.push("This would not cause a crash...");
-    ML::sleep(1.0);
-
-    if (!doCrash) {
-        loop.shutdown();
-    }
-}
 
 /* This test ensures that adding sources works correctly when needsPoll is
  * set. Otherwise, the watchdog will be triggered. */
