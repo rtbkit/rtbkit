19f065203cde8bf560cdb402cd0f4fee5fc1c72d
Mich <fmlheureux@datacratic.com>
2014-06-03 15:09:03 -0400

    PLAT-546 SnsApiWrapper, MockSnsApiWrapper, test for MocksnsApiWrapper

--- service/testing/service_testing.mk
+++ service/testing/service_testing.mk
@@ -46,3 +46,5 @@ $(eval $(call test,http_client_test,services test_services,boost))
 $(eval $(call test,http_client_bench,boost_program_options services test_services,boost manual))
 
 $(eval $(call test,logs_test,services,boost))
+
+$(eval $(call test,sns_mock_test,cloud services,boost))
