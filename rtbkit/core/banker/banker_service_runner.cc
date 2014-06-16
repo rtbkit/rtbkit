45c17c208489a91e668b9d6d2a788a3bdb443389
Mathieu Stefani <mathieu@datacratic.com>
2014-06-16 14:48:34 -0400

    banker_service_runner now takes back ZooKeeper into account

--- rtbkit/core/banker/banker_service_runner.cc
+++ rtbkit/core/banker/banker_service_runner.cc
@@ -48,7 +48,7 @@ int main(int argc, char ** argv)
 
     options_description all_opt;
     all_opt
-        .add(serviceArgs.makeProgramOptions("General Options", NO_ZOOKEEPER))
+        .add(serviceArgs.makeProgramOptions("General Options"))
         .add(configuration_options);
     all_opt.add_options()
         ("help,h", "print this message");
@@ -66,7 +66,7 @@ int main(int argc, char ** argv)
         exit(1);
     }
 
-    auto proxies = serviceArgs.makeServiceProxies(CS_NULL);
+    auto proxies = serviceArgs.makeServiceProxies();
     auto serviceName = serviceArgs.serviceName("masterBanker");
 
     MasterBanker banker(proxies, serviceName);
