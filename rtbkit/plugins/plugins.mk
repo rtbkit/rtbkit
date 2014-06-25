904f909eef6544fee7de89c1caca426589d024e2
jsbejeau <jsbejeau@datacratic.com>
2014-06-25 10:03:43 -0400

    Fix build ordering

--- rtbkit/plugins/plugins.mk
+++ rtbkit/plugins/plugins.mk
@@ -1,9 +1,9 @@
 # RTBKIT plugins makefile
 
 $(eval $(call include_sub_make,bidding_agent))
-$(eval $(call include_sub_make,bidder_interface))
 $(eval $(call include_sub_make,rtb_augmentor,augmentor,rtb_augmentor.mk))
 $(eval $(call include_sub_make,data_logger))
 $(eval $(call include_sub_make,bid_request))
+$(eval $(call include_sub_make,bidder_interface))
 $(eval $(call include_sub_make,exchange))
 $(eval $(call include_sub_make,adserver))
