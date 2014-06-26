06eb02bc66e97c16ad8222db0bbf25597e0c0165
jsbejeau <jsbejeau@datacratic.com>
2014-06-26 17:08:35 -0400

    Fix typo

--- rtbkit/core/router/router.h
+++ rtbkit/core/router/router.h
@@ -107,7 +107,7 @@ struct Router : public ServiceBase,
            bool logAuctions = false,
            bool logBids = false,
            Amount maxBidAmount = USD_CPM(200),
-           int secondsUntileSlowMode = 2);
+           int secondsUntilSlowMode = 2);
 
     Router(std::shared_ptr<ServiceProxies> services = std::make_shared<ServiceProxies>(),
            const std::string & serviceName = "router",
@@ -116,7 +116,7 @@ struct Router : public ServiceBase,
            bool logAuctions = false,
            bool logBids = false,
            Amount maxBidAmount = USD_CPM(200),
-           int secondsUntileSlowMode = 2);
+           int secondsUntilSlowMode = 2);
 
     ~Router();
 
