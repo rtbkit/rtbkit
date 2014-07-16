7d5c40854790340fb079255ef5c1ae72c6bced7d
Wolfgang Sourdeau <Wolfgang@Contre.COM>
2014-07-16 10:27:01 -0400

    bidder_interface.mk: added dependency of librtb on bidder_interface plugins

--- rtbkit/plugins/bidder_interface/bidder_interface.mk
+++ rtbkit/plugins/bidder_interface/bidder_interface.mk
@@ -1,4 +1,5 @@
+$(eval $(call library,agents_bidder,agents_bidder_interface.cc,))
+$(eval $(call library,http_bidder,http_bidder_interface.cc,openrtb_bid_request))
 
-$(eval $(call library,agents_bidder,agents_bidder_interface.cc,rtb_router))
-$(eval $(call library,http_bidder,http_bidder_interface.cc,rtb_router openrtb_bid_request))
-
+# the code loading the plugins above is part of librtb (bidder_interface.cc)
+$(LIB)/librtb.so: $(LIB)/libagents_bidder.so $(LIB)/libhttp_bidder.so
