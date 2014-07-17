bc8f1c7391bd93150087a147b56731cf50a740d7
Wolfgang Sourdeau <wolfgang@datacratic.com>
2014-07-17 14:45:00 -0400

    let the "router_runner" and "post_auction_runner" depend on the bidder interface plugins, rather than librtb.so, as the latter form seems to confuse the make system

--- rtbkit/plugins/bidder_interface/bidder_interface.mk
+++ rtbkit/plugins/bidder_interface/bidder_interface.mk
@@ -1,5 +1,4 @@
-$(eval $(call library,agents_bidder,agents_bidder_interface.cc,))
+$(eval $(call library,agents_bidder,agents_bidder_interface.cc,rtb_router))
 $(eval $(call library,http_bidder,http_bidder_interface.cc,openrtb_bid_request))
 
-# the code loading the plugins above is part of librtb (bidder_interface.cc)
-$(LIB)/librtb.so: $(LIB)/libagents_bidder.so $(LIB)/libhttp_bidder.so
+$(BIN)/router_runner $(BIN)/post_auction_runner: $(LIB)/libagents_bidder.so $(LIB)/libhttp_bidder.so
