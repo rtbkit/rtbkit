64570307c22fc4fa845451c09050ecb1e23a1cf1
Wolfgang Sourdeau <wolfgang@datacratic.com>
2014-07-17 16:24:48 -0400

    added "bidder_interface_plugins" as a phony target

--- rtbkit/plugins/bidder_interface/bidder_interface.mk
+++ rtbkit/plugins/bidder_interface/bidder_interface.mk
@@ -1,4 +1,8 @@
 $(eval $(call library,agents_bidder,agents_bidder_interface.cc,rtb_router))
 $(eval $(call library,http_bidder,http_bidder_interface.cc,openrtb_bid_request))
 
-$(BIN)/router_runner $(BIN)/post_auction_runner: $(LIB)/libagents_bidder.so $(LIB)/libhttp_bidder.so
+
+bidder_interface_plugins: $(LIB)/libagents_bidder.so $(LIB)/libhttp_bidder.so
+
+
+.PHONY: bidder_interface_plugins
