4d35cd2517c15d2c515fefe9f8b0d75a58bffa77
Nicolas Emiliani <nicolas@motrixi.com>
2014-09-22 11:35:33 +0000

    http requester stops when done

--- soa/service/testing/http_requester.cc
+++ soa/service/testing/http_requester.cc
@@ -10,6 +10,7 @@
 
 #define BASE_URL        "http://api-east1-aws.appaudience.com"
 #define CONCURRENT_REQS 1
+#define NUM_REQS        1
 
 using namespace std;
 using namespace Datacratic;
@@ -19,7 +20,7 @@ int main(int argc, char* argv[]){
     MessageLoop loop;
     loop.start();
 
-    int done(false);
+    int num_reqs(0), num_resps(0);
 
     //create the http client
     auto client = std::make_shared<HttpClient>(
@@ -27,17 +28,18 @@ int main(int argc, char* argv[]){
     
     loop.addSource("httpClient", client);
 
-    for(auto i : {0,1,2,3} ){
+    for(auto i = 0; i < NUM_REQS; i++){
 
         // create the callback    
         auto onResponse = [&,i] ( const HttpRequest & rq, 
                                 HttpClientError errorCode_,
                                 int status, string && headers, string && body) {
             cout << "received response " << i << endl;
-            //cout << "body : " << body << endl;
+            cout << "body : " << body << endl;
             cout << "errorCode_ : " << errorCode_ << endl;
             cout << "--------------" << endl;
-            ML::futex_wake(done);
+            ML::futex_wake(num_reqs);
+            ++num_resps;
         };
         auto cbs = make_shared<HttpClientSimpleCallbacks>(onResponse);
 
@@ -45,13 +47,15 @@ int main(int argc, char* argv[]){
                  RestParams({
                     {"Content-Type", "application/json"},
                     {"Connection", "keep-alive"}
-                }), -1);
+                }), 1000);
         
     }
-    while(!done){
-        int old(done);
+    while(!num_reqs){
+        int old(num_reqs);
         cout << "waiting ... " << endl;
-        ML::futex_wait(done, old);
+        ML::futex_wait(num_reqs, old);
+        if(num_resps == NUM_REQS)
+            num_reqs = true;
     }
     loop.removeSource(client.get());
     return 0;
