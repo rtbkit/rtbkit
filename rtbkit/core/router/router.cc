ebdaa2dba320d871f4b3224c1d84d044fa904d6c
Jean-Michel Bouchard <jim@polarcoordinates.org>
2014-10-24 10:34:22 -0400

    changed slowModeActive for a better name

--- rtbkit/core/router/router.cc
+++ rtbkit/core/router/router.cc
@@ -147,7 +147,7 @@ Router(ServiceBase & parent,
       numAuctionsWithBid(0), numNoPotentialBidders(0),
       numNoBidders(0),
       monitorClient(getZmqContext(), secondsUntilSlowMode),
-      slowModeActive(false),
+      slowModePeriodicSpentReached(false),
       slowModeAuthorizedMoneyLimit(slowModeAuthorizedMoneyLimit),
       accumulatedBidMoneyInThisPeriod(0),
       monitorProviderClient(getZmqContext()),
@@ -197,7 +197,7 @@ Router(std::shared_ptr<ServiceProxies> services,
       numAuctionsWithBid(0), numNoPotentialBidders(0),
       numNoBidders(0),
       monitorClient(getZmqContext(), secondsUntilSlowMode),
-      slowModeActive(false),
+      slowModePeriodicSpentReached(false),
       slowModeAuthorizedMoneyLimit(slowModeAuthorizedMoneyLimit),
       accumulatedBidMoneyInThisPeriod(0),
       monitorProviderClient(getZmqContext()),
@@ -1927,7 +1927,7 @@ doBidImpl(const BidMessage &message, const std::vector<std::string> &originalMes
         }
 
         if (bid.price.isNegative() || bid.price > maxBidAmount) {
-            if (slowModeActive) {
+            if (slowModePeriodicSpentReached) {
                 bid.price = maxBidAmount;
             } else {
                 returnInvalidBid(agent, bidsString, auctionInfo.auction,
@@ -1984,7 +1984,7 @@ doBidImpl(const BidMessage &message, const std::vector<std::string> &originalMes
             if ((uint32_t) slowModeLastAuction.secondsSinceEpoch()
                     < (uint32_t) now.secondsSinceEpoch()) {
                 slowModeLastAuction = now;
-                slowModeActive = false;
+                slowModePeriodicSpentReached = false;
                 // TODO Insure in router.cc (not router_runner) that
                 // maxBidPrice <= slowModeAuthorizedMoneyLimit
                 // Here we're garanteed that price.value >= slowModeAuthorizedMoneyLimit
@@ -1998,16 +1998,16 @@ doBidImpl(const BidMessage &message, const std::vector<std::string> &originalMes
                 // Check if we're spending more in this period than what slowModeAuthorizedMoneyLimit
                 // allows us to.
                 if (accumulatedBidMoneyInThisPeriod > slowModeAuthorizedMoneyLimit.value) {
-                    slowModeActive = true;
+                    slowModePeriodicSpentReached = true;
                     bidder->sendBidDroppedMessage(agentConfig, agent, auctionInfo.auction);
                     recordHit("slowMode.droppedBid");
                 continue;
                 }
             }
         } else {
-            // Make sure slowModeActive is false if monitor success is satisfied.
-            // There is a possible (90% sure..) code path where slowModeActive is not resetted
-            slowModeActive = false;
+            // Make sure slowModePeriodicSpentReached is false if monitor success is satisfied.
+            // There is a possible (90% sure..) code path where slowModePeriodicSpentReached is not resetted
+            slowModePeriodicSpentReached = false;
         }
 
         if (!banker->authorizeBid(config.account, auctionKey, price)
@@ -2338,7 +2338,7 @@ onNewAuction(std::shared_ptr<Auction> auction)
     if (!monitorClient.getStatus(slowModeTolerance)) {
         // check if slow mode active and in the same second then ignore the Auction
         Date now = Date::now();
-        if (slowModeActive && (uint32_t) slowModeLastAuction.secondsSinceEpoch()
+        if (slowModePeriodicSpentReached && (uint32_t) slowModeLastAuction.secondsSinceEpoch()
                 == (uint32_t) now.secondsSinceEpoch() ) {
             recordHit("monitor.ignoredAuctions");
             auction->finish();
