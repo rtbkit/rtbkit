ac4f36dce8e3b30040e6345dcd4dc375c8161d86
Michael Burkat <mike.burkat@gmail.com>
2014-11-06 11:04:00 -0500

    removed nodejs dependent modules from compiling

--- soa/js/js.mk
+++ soa/js/js.mk
@@ -4,15 +4,15 @@
 #
 # Support functions for javascript
 
-LIBJS_SOURCES := \
-	js_wrapped.cc \
-	js_value.cc \
-	js_utils.cc \
-	js_registry.cc \
-	js_call.cc
+#LIBJS_SOURCES := \
+#	js_wrapped.cc \
+#	js_value.cc \
+#	js_utils.cc \
+#	js_registry.cc \
+#	js_call.cc
 
-LIBJS_LINK := jsoncpp $(NODE_V8_LIB) node_exception_tracing arch utils types
+#LIBJS_LINK := jsoncpp $(NODE_V8_LIB) node_exception_tracing arch utils types
 
-$(eval $(call library,js,$(LIBJS_SOURCES),$(LIBJS_LINK)))
+#$(eval $(call library,js,$(LIBJS_SOURCES),$(LIBJS_LINK)))
 
-$(eval $(call include_sub_make,js_testing,testing,js_testing.mk))
+#$(eval $(call include_sub_make,js_testing,testing,js_testing.mk))
