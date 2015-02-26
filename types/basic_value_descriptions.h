376968ef470f83d7b13130b92cc485356fa3af93
Eric Robert <eric@datacratic.com>
2015-02-26 12:58:09 -0500

    added support for utf-8 in Id

--- types/basic_value_descriptions.h
+++ types/basic_value_descriptions.h
@@ -36,7 +36,7 @@ struct DefaultDescription<Datacratic::Id>
             val->val2 == 0 && val->val1 <= std::numeric_limits<int32_t>::max()) {
             context.writeInt(val->val1);
         } else {
-            context.writeString(val->toString());
+            context.writeStringUtf8(Utf8String(val->toString()));
         }
     }
 
