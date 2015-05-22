9bf4d78005394f11b0d05de212ce4a8e381eca3e
Wolfgang Sourdeau <Wolfgang@Contre.COM>
2015-05-22 17:58:05 -0400

    PLAT-900: declare the "format-string" version of ML::Exception as such

--- jml/arch/exception.h
+++ jml/arch/exception.h
@@ -25,6 +25,7 @@
 
 #include <string>
 #include <exception>
+#include "jml/compiler/compiler.h"
 #include "stdarg.h"
 #include "exception_handler.h"
 
@@ -33,7 +34,7 @@ namespace ML {
 class Exception : public std::exception {
 public:
     Exception(const std::string & msg);
-    Exception(const char * msg, ...);
+    Exception(const char * msg, ...) JML_FORMAT_STRING(2, 3);
     Exception(const char * msg, va_list ap);
     Exception(int errnum, const std::string & msg, const char * function = 0);
     virtual ~Exception() throw();
