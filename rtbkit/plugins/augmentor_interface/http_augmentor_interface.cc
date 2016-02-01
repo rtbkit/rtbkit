71e7dbdcd75c9dbfc2be8dc396e495a53de5695c
Nicolas Emiliani <nicolas@motrixi.com>
2016-02-01 19:02:36 -0300

    Add the code for the http augmentation response callback.

--- rtbkit/plugins/augmentor_interface/http_augmentor_interface.cc
+++ rtbkit/plugins/augmentor_interface/http_augmentor_interface.cc
@@ -8,6 +8,7 @@
 #include "soa/service/http_client.h"
 #include "rtbkit/plugins/bid_request/openrtb_bid_request_parser.h"
 #include "rtbkit/openrtb/openrtb_parsing.h"
+#include "jml/arch/timers.h"
 
 using namespace Datacratic;
 
@@ -60,7 +61,7 @@ HttpAugmentorInterface::HttpAugmentorInterface(
             auto client = std::make_shared<HttpClient>(address);
             info.push_back(std::make_shared<HttpAugmentorInstanceInfo>(maxInFlight, client, path, name));
         }
-        augmentors[augmentorName] = info;
+        augmentors[augmentorName] = std::move(info);
     }
 }
 
@@ -115,11 +116,53 @@ HttpAugmentorInterface::sendAugmentMessage(
 
     RestParams headers { { "X-Openrtb-Version", openrtbVersion } };
 
+    std::string aid = auction->id.toString();
+    std::string addr = instance->address();
+
     auto callback = std::make_shared<HttpClientSimpleCallbacks>(
-            [=](const HttpRequest &, HttpClientError errorCode,
-                int statusCode, const std::string &, std::string &&body)
+            [=](const HttpRequest& request, HttpClientError errorCode,
+                int statusCode, const std::string& headers, std::string &&body)
             {
-                /* To be continued */
+                std::string augmentor;
+                Datacratic::Date startTime;
+
+                if(errorCode == HttpClientError::None && statusCode == 200){
+                    try{
+                        RestParams headers_;
+                        headers_ = RestParams::fromBinary(headers);
+
+                        // get the version
+                        std::string version = headers_.getValue("X-Rtbkit-Protocol-Version");
+                        ExcCheckEqual(version, "1.0", "unknown response version");
+
+                        // get the timestamp
+                        std::string t = headers_.getValue("X-Rtbkit-Timestamp");
+                        startTime = Date::parseSecondsSinceEpoch(t);
+
+                        // get the auction id
+                        std::string auctionid = headers_.getValue("X-Rtbkit-Auction-Id");
+                        ExcCheckEqual(aid, auctionid, "auction id is not the same");
+                        Id id(auctionid);
+
+                        // get all the augmentation data
+                        augmentor = headers_.getValue("X-Rtbkit-Augmentor-Name");
+                        ExcCheckEqual(name, augmentor, "augmentor name is not the same");
+
+                    }catch(...){
+                        augmentor = name;
+                        recordHit("augmentor.%s.exceptionOnResponse", augmentor);
+                    }
+                }else{
+                    augmentor = name;
+                }
+                Id id(aid);
+                onResponse(
+                    AugmentationResponse(
+                        std::move(addr),
+                        startTime,
+                        id,
+                        std::move(augmentor),
+                        std::move(body)));
             }
     );
 
