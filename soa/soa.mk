5bfa557db6d313f6c27e7cff9c9c58f12711ac5e
Michael Burkat <mike.burkat@gmail.com>
2016-05-31 13:15:44 -0400

    fix NODEJS_ENABLED flag in soa

--- soa/soa.mk
+++ soa/soa.mk
@@ -2,10 +2,10 @@
 
 $(eval $(call include_sub_make,jsoncpp))
 $(eval $(call include_sub_make,types))
-$(eval $(call include_sub_make,sync))
-ifeq ($NODEJS_ENABLED),1)
+ifeq ($(NODEJS_ENABLED),1)
 $(eval $(call include_sub_make,js))
 $(eval $(call include_sub_make,sigslot))
+$(eval $(call include_sub_make,sync))
 endif
 $(eval $(call include_sub_make,gc))
 $(eval $(call include_sub_make,service))
